b20712aaa4a47b6b119ed81434ba01f3fa08db47
hub33k <hub33k@yahoo.com>
2023-08-14 11:06:06 +0200

    chore: bump @elpassion/taco to 0.1.71

--- apps/web/src/modules/Layout/Sidebar.tsx
+++ apps/web/src/modules/Layout/Sidebar.tsx
@@ -4,13 +4,14 @@ import React from 'react';
 import { ResponsiveSidebar } from '@elpassion/taco';
 
 export const Sidebar = () => {
+  const [isOpen, setIsOpen] = React.useState(false);
+
   return (
     <ResponsiveSidebar
-      // TODO (hub33k): fix this is design system
-      //   isOpen - is passed to Sidebar
-      //   close - hydration error
-      // isOpen={isOpen}
-      close={close} //  NOTE (hub33k): without this - Warning: Did not expect server HTML to contain a <div> in <div> + Hydration failed
+      isOpen={isOpen}
+      close={() => {
+        setIsOpen(!isOpen);
+      }}
       sidebarClassName="sticky top-0 bg-white border-r border-gray-200"
       drawerClassName="pt-[68px]"
       collapseBtnClassName="absolute top-14 -right-2"
