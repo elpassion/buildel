e590d5d2a340e8c8d09786548da714c9a8aae33a
Michał Warda <michal.warda@elpassion.pl>
2023-08-14 14:22:52 +0200

    Change example

--- apps/web/src/env.mjs
+++ apps/web/src/env.mjs
@@ -2,10 +2,12 @@ import z from 'zod';
 
 const envSchema = z.object({
   WEBSOCKET_URL: z.string().url(),
+  API_URL: z.string().url(),
 });
 
 const envs = {
   WEBSOCKET_URL: process.env.WEBSOCKET_URL,
+  API_URL: process.env.API_URL,
 };
 
 export const ENV = envSchema.parse(envs);
--- apps/web/src/modules/Example/ExampleClient.tsx
+++ apps/web/src/modules/Example/ExampleClient.tsx
@@ -1,112 +1,155 @@
 'use client';
 
+import { Card } from '@elpassion/taco';
 import React from 'react';
-import Image from 'next/image';
-import { Button } from '@elpassion/taco';
-import {
-  AudioInputBlock,
-  ChatBlock,
-  SpeechToTextBlock,
-  TextToSpeechBlock,
-  audioInputBlock,
-  chatBlock,
-  ppush,
-  speechToTextBlock,
-  textToSpeechBlock,
-  useBlocks,
-} from '~/modules/Blocks';
+import z from 'zod';
+import { ENV } from '~/env.mjs';
+import { ppush, useBlocks } from '~/modules/Blocks';
+import { useEffectOnce } from '~/utils/hooks';
+
+const IOType = z.object({ name: z.string(), type: z.enum(['audio', 'text']) });
+
+const BlockType = z.object({
+  type: z.string(),
+  inputs: z.array(IOType),
+  outputs: z.array(IOType),
+  schema: z.string(),
+});
+
+const BlockTypes = z.array(BlockType);
+
+const BlockTypesResponse = z.object({
+  data: BlockTypes,
+});
+
+const Block = z.object({
+  name: z.string(),
+  forward_outputs: z.array(z.string()),
+  opts: z.object({}),
+  config: BlockType,
+});
+
+const BlocksResponse = z.object({
+  blocks: z.array(Block),
+});
+
+type JSONSchemaField =
+  | {
+      type: 'object';
+      properties: { [key: string]: JSONSchemaField };
+      required?: string[];
+    }
+  | {
+      type: 'string';
+      title: string;
+      description: string;
+      minLength?: number;
+    }
+  | {
+      type: 'number';
+      title: string;
+      description: string;
+      minimum?: number;
+      maximum?: number;
+    }
+  | {
+      type: 'array';
+      title: string;
+      description: string;
+      items: JSONSchemaField;
+    };
 
 export const ExampleClient = () => {
   const [{ channel }] = useBlocks();
-  const [blocks, setBlocks] = React.useState<any>([]);
+  const [blocks, setBlocks] = React.useState<z.infer<typeof Block>[]>([]);
+  const [blockTypes, setBlockTypes] = React.useState<
+    z.infer<typeof BlockTypes>
+  >([]);
+  useEffectOnce(() => {
+    const getBlockTypes = async () => {
+      const response = await fetch(`http://${ENV.API_URL}/api/block_types`);
+      const data = await response.json();
+      const blockTypesResponse = BlockTypesResponse.parse(data);
+      setBlockTypes(blockTypesResponse.data);
+    };
+    getBlockTypes();
+  });
 
-  const blockList = React.useMemo(() => {
-    return [
-      {
-        name: audioInputBlock.name,
-        img: 'https://pbblogassets.s3.amazonaws.com/uploads/2015/08/Audio-Waveforms-Featued-Image.jpg',
-        data: audioInputBlock,
-        Component: AudioInputBlock,
+  const addBlock = async (blockType: string) => {
+    channel.push('add_block', {
+      name: blockType,
+      type: blockType,
+      opts: {
+        input: '',
+        messages: [],
       },
-      {
-        name: speechToTextBlock.name,
-        img: 'https://nordicapis.com/wp-content/uploads/5-Best-Speech-to-Text-APIs-2-e1615383933700-1024x573.png',
-        data: speechToTextBlock,
-        Component: SpeechToTextBlock,
-      },
-      {
-        name: chatBlock.name,
-        img: 'https://images.unsplash.com/photo-1684487747720-1ba29cda82f8?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1332&q=80',
-        data: chatBlock,
-        Component: ChatBlock,
-      },
-      {
-        name: textToSpeechBlock.name,
-        img: 'https://uploads-ssl.webflow.com/5f57aae9cf84e83399ba0032/62b264ced6e2c5184b2ce2d4_The%208%20Best%20TTS%20Voice%20Providers.jpg',
-        data: textToSpeechBlock,
-        Component: TextToSpeechBlock,
-      },
-    ];
-  }, []);
+      forward_outputs: [],
+    });
+    const blocks = await ppush(channel, 'get_blocks', {});
+    const validatedBlocks = BlocksResponse.parse(blocks);
+    setBlocks(validatedBlocks.blocks);
+  };
+
+  function renderSchema(schema: string) {
+    const schemaObj = JSON.parse(schema);
+    return renderField(schemaObj, null, schemaObj);
+  }
+
+  function renderField(
+    field: JSONSchemaField,
+    key: string | null,
+    schema: JSONSchemaField,
+  ) {
+    if (field.type === 'string') {
+      return (
+        <div>
+          {field.title} - {field.description}
+          <input name={key!} type="text" />
+        </div>
+      );
+    } else if (field.type === 'number') {
+      return (
+        <div>
+          {field.title} - {field.description}
+          <input name={key!} type="number" />
+        </div>
+      );
+    } else if (field.type === 'object') {
+      return Object.entries(field.properties).map(([propertyKey, value]) => {
+        const fieldKey = key === null ? propertyKey : `${key}.${propertyKey}`;
+        return <div key={key}>{renderField(value, fieldKey, schema)}</div>;
+      });
+    } else if (field.type === 'array') {
+      const fieldKey = key === null ? '0' : `${key}.0`;
+      return (
+        <div>
+          {field.title} - {field.description}
+          {renderField(field.items, fieldKey, schema)}
+        </div>
+      );
+    }
+    console.warn('Unknown field type', field);
+    return null;
+  }
 
   return (
     <>
       <div className="">
-        <div className="flex gap-4">
-          <Button
-            text="Reset"
-            onClick={() => {
-              channel.push('remove_block', { name: 'audio_input' });
-              channel.push('remove_block', { name: 'speech_to_text' });
-              channel.push('remove_block', { name: 'chat' });
-              channel.push('remove_block', { name: 'text_to_speech' });
-
-              channel.push('reset_blocks', {});
-
-              setBlocks([]);
-            }}
-          />
-
-          <Button
-            text="Get blocks"
-            onClick={() => {
-              ppush(channel, 'get_blocks', {}).then(console.log);
-            }}
-          />
-        </div>
-
-        <div className="mb-8" />
-
-        <p className="text-2xl">Add block</p>
-
+        Add block
         <div className="mb-4" />
-
         <div className="grid cursor-pointer grid-cols-4 gap-4">
-          {blockList.map((block) => (
-            <div
-              key={block.name}
-              onClick={() => {
-                setBlocks((prevState: any) => [...prevState, block]);
-              }}
-            >
-              <div>
-                {/* eslint-disable-next-line @next/next/no-img-element */}
-                <img src={block.img} height={160} alt="Audio input" />
-              </div>
-
-              <p>{block.name.replaceAll('_', ' ').toUpperCase()}</p>
-            </div>
+          {blockTypes.map((blockType) => (
+            <Card key={blockType.type}>
+              {blockType.type.replaceAll('_', ' ').toUpperCase()}
+            </Card>
           ))}
         </div>
-
         <div className="mb-12" />
-
         <div className="grid grid-cols-4 gap-4">
-          {blocks.map((block: (typeof blockList)[number], index: number) => {
-            const Comp = block.Component;
+          {blocks.map((block, index: number) => {
             return (
               <div key={`${block.name}-${index}`}>
-                <Comp />
+                {renderSchema(block.config.schema)}
               </div>
             );
           })}
