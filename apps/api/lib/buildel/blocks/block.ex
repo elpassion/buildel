b3173a4fc57afc07dedd73d8d721cdb1196df3aa
Michał Warda <michal.warda@elpassion.pl>
2023-08-30 10:26:38 +0200

    Extract stream_state to utils

--- apps/api/lib/buildel/blocks/block.ex
+++ apps/api/lib/buildel/blocks/block.ex
@@ -35,6 +35,7 @@ defmodule Buildel.Blocks.Block do
   defmacro __using__(_opts) do
     quote do
       use GenServer
+      use Buildel.Blocks.Utils.StreamState
       alias Buildel.Blocks.Block
       alias Buildel.BlockPubSub
       defstruct [:name, :type, opts: %{}]
@@ -95,71 +96,6 @@ defmodule Buildel.Blocks.Block do
         |> Enum.map(fn input ->
           BlockPubSub.subscribe_to_io(context_id, input)
         end)
-      end
-
-      defp assign_stream_state(state) do
-        state
-        |> Keyword.put(:stream_state, :idle)
-        |> Keyword.put(:stream_timer, nil)
-      end
-
-      defp send_stream_start(state, output \\ "output") do
-        case state[:stream_state] do
-          :idle ->
-            state = put_in(state[:stream_state], :streaming)
-
-            BlockPubSub.broadcast_to_io(
-              state[:context_id],
-              state[:block_name],
-              output,
-              {:start_stream, nil}
-            )
-
-            BlockPubSub.broadcast_to_block(
-              state[:context_id],
-              state[:block_name],
-              {:start_stream, nil}
-            )
-
-            state
-
-          _ ->
-            state
-        end
-      end
-
-      defp send_stream_stop(state, output \\ "output") do
-        case state[:stream_state] do
-          :streaming ->
-            state = put_in(state[:stream_state], :idle)
-
-            BlockPubSub.broadcast_to_io(
-              state[:context_id],
-              state[:block_name],
-              output,
-              {:stop_stream, nil}
-            )
-
-            BlockPubSub.broadcast_to_block(
-              state[:context_id],
-              state[:block_name],
-              {:stop_stream, nil}
-            )
-
-            state
-
-          _ ->
-            state
-        end
-      end
-
-      defp schedule_stream_stop(state, output \\ "output") do
-        if state[:stream_timer] && state[:stream_state] == :streaming do
-          Process.cancel_timer(state[:stream_timer])
-        end
-
-        timer = Process.send_after(self(), {:stop_stream, output}, stream_timeout())
-        put_in(state[:stream_timer], timer)
       end
 
       defp stream_timeout() do
