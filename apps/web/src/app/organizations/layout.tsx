78566aa0ac921104fa90dac044311381abcd84db
kielbasa-elp <dawid.kielbasa@elpassion.pl>
2023-09-07 12:30:11 +0200

    Fix other pages

--- apps/web/src/app/organizations/layout.tsx
+++ apps/web/src/app/organizations/layout.tsx
@@ -1,9 +1,16 @@
 import React from 'react';
 import { Layout } from '~/modules/Layout';
-export default function OrganizationLayout({
+import { OrganizationsApi } from '~api/Organizations/OrganizationsApi';
+export default async function OrganizationLayout({
   children,
 }: {
   children: React.ReactNode;
 }) {
+  const organizationApi = new OrganizationsApi();
+
+  // const organizations = await organizationApi.getAll();
+  //
+  // console.log(organizations);
+
   return <Layout>{children}</Layout>;
 }
--- apps/web/src/modules/Pages/DashboardPage/DashboardPage.tsx
+++ apps/web/src/modules/Pages/DashboardPage/DashboardPage.tsx
@@ -1,14 +1,14 @@
-import { MainContainer } from '~/modules/Layout';
+import { Layout, MainContainer } from '~/modules/Layout';
 import { DashboardNavbar } from './DashboardNavbar';
 
 export const DashboardPage = () => {
   return (
-    <>
+    <Layout>
       <DashboardNavbar />
 
       <MainContainer>
         <p className="text-2xl">Dashboard page</p>
       </MainContainer>
-    </>
+    </Layout>
   );
 };
