df8289506547248fefc9c83addb16cf0ed9e1678
Michał Warda <michal.warda@elpassion.pl>
2023-09-08 17:24:12 +0200

    Add reactflow

--- apps/web-remix/app/components/pages/pipelines/show/page.tsx
+++ apps/web-remix/app/components/pages/pipelines/show/page.tsx
@@ -1,8 +1,47 @@
 import { useLoaderData } from "@remix-run/react";
 import { loader } from "./loader";
+import ReactFlow, {
+  Background,
+  BackgroundVariant,
+  Connection,
+  Controls,
+  EdgeChange,
+  NodeChange,
+  addEdge,
+  applyEdgeChanges,
+  applyNodeChanges,
+} from "reactflow";
+import { LinksFunction, V2_MetaFunction } from "@remix-run/node";
+
+import flowStyles from "reactflow/dist/style.css";
+
+export const links: LinksFunction = () => [
+  { rel: "stylesheet", href: flowStyles },
+];
 
 export function ShowPipelinePage() {
   const { pipeline } = useLoaderData<typeof loader>();
 
-  return <div>{pipeline.name}</div>;
+  return (
+    <div className="h-screen w-full">
+      <ReactFlow
+        fitViewOptions={{
+          minZoom: 0.5,
+          maxZoom: 1,
+        }}
+        fitView
+      >
+        <Background variant={BackgroundVariant.Lines} />
+        <Controls />
+      </ReactFlow>
+    </div>
+  );
 }
+
+export const meta: V2_MetaFunction = () => {
+  return [
+    {
+      title: "Show pipeline",
+    },
+  ];
+};
