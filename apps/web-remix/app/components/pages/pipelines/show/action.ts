170d6f3dea7433d0d4e8f85cf13aaff59a82ca47
Michał Warda <michal.warda@elpassion.pl>
2023-09-08 18:21:36 +0200

    Add updating pipeline on backend :O

--- apps/web-remix/app/components/pages/pipelines/show/action.ts
+++ apps/web-remix/app/components/pages/pipelines/show/action.ts
@@ -2,9 +2,10 @@ import { ActionArgs, redirect } from "@remix-run/node";
 import { withZod } from "@remix-validated-form/with-zod";
 import invariant from "tiny-invariant";
 import { requireLogin } from "~/session.server";
-import { actionBuilder } from "~/utils.server";
+import { actionBuilder, fetchTyped } from "~/utils.server";
 import { schema } from "./schema";
 import { validationError } from "remix-validated-form";
+import { PipelineResponse } from "../list/contracts";
 
 export async function action(actionArgs: ActionArgs) {
   return actionBuilder({
@@ -18,6 +19,21 @@ export async function action(actionArgs: ActionArgs) {
       const result = await validator.validate(await actionArgs.request.json());
 
       if (result.error) return validationError(result.error);
+
+      await fetchTyped(
+        PipelineResponse,
+        `http://127.0.0.1:4000/api/organizations/${params.organizationId}/pipelines/${params.pipelineId}`,
+        {
+          headers: {
+            "Content-Type": "application/json",
+            Cookie: request.headers.get("cookie")!,
+          },
+          method: "PUT",
+          body: JSON.stringify({
+            pipeline: result.data,
+          }),
+        }
+      );
 
       return redirect(
         `/${params.organizationId}/pipelines/${params.pipelineId}`
