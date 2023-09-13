338a0e8597b949cef9c19727e9bc7591ca4659dc
Michał Warda <michal.warda@elpassion.pl>
2023-09-13 11:30:39 +0200

    Remove authorization from socket

--- apps/api/lib/buildel_web/pipeline_socket.ex
+++ apps/api/lib/buildel_web/pipeline_socket.ex
@@ -1,30 +1,12 @@
 defmodule BuildelWeb.PipelineSocket do
-  alias Buildel.Organizations
   use Phoenix.Socket
-  use BuildelWeb.Validator
 
   channel "pipeline_runs:*", BuildelWeb.PipelineRunChannel
   channel "pipelines:*", BuildelWeb.PipelineChannel
 
-  defparams :connect do
-    required(:api_key, :string)
-    required(:organization_id, :integer)
+  def connect(_params, socket, _connect_info) do
+    {:ok, socket}
   end
 
-  def connect(params, socket, _connect_info) do
-    with {:ok, %{api_key: api_key, organization_id: organization_id}} <-
-           validate(:connect, params),
-         {:ok, organization} <-
-           Organizations.get_organization_by_id_and_api_key(organization_id, api_key) do
-      {:ok, socket |> assign(:organization, organization)}
-    else
-      {:error, %Ecto.Changeset{} = changeset} ->
-        {:error, BuildelWeb.ChangesetJSON.error(%{changeset: changeset})}
-
-      _err ->
-        {:error, %{errors: %{detail: "Not Found"}}}
-    end
-  end
-
-  def id(_socket), do: nil
+  def id(_socket), do: UUID.uuid4()
 end
--- apps/api/test/buildel_web/channels/pipeline_socket_test.exs
+++ apps/api/test/buildel_web/channels/pipeline_socket_test.exs
@@ -1,42 +1,10 @@
 defmodule BuildelWeb.PipelineSocketTest do
-  alias Buildel.Organizations.Organization
   use BuildelWeb.ChannelCase
 
-  import Buildel.OrganizationsFixtures
-
   describe "connect" do
-    test "fails when trying to connect without correct params" do
-      assert {:error,
-              %{
-                errors: %{api_key: ["can't be blank"], organization_id: ["can't be blank"]}
-              }} =
-               BuildelWeb.PipelineSocket |> connect(%{})
-    end
-
-    test "fails when trying to connect to non existant organization" do
-      assert {:error, %{errors: %{detail: "Not Found"}}} =
-               BuildelWeb.PipelineSocket
-               |> connect(%{organization_id: 123, api_key: "api_key"})
-    end
-
-    test "fails when trying to connect to existant org with wrong key" do
-      organization = organization_fixture()
-
-      assert {:error, %{errors: %{detail: "Not Found"}}} =
-               BuildelWeb.PipelineSocket
-               |> connect(%{organization_id: organization.id, api_key: "wrong_key"})
-    end
-
-    test "connects with correct data" do
-      organization = organization_fixture()
-      organization_id = organization.id
-
-      assert {:ok, %Phoenix.Socket{assigns: %{organization: %Organization{id: ^organization_id}}}} =
-               BuildelWeb.PipelineSocket
-               |> connect(%{
-                 organization_id: organization.id,
-                 api_key: organization.api_keys |> List.first() |> Map.get(:key)
-               })
+    test "connects and assigns id to user" do
+      {:ok, %Phoenix.Socket{id: id}} = BuildelWeb.PipelineSocket |> connect(%{})
+      assert id != nil
     end
   end
 end
