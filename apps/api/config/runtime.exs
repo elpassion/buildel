37a931f5abed53b78e718ceeacefffae6e1691c0
Michał Warda <michal.warda@elpassion.pl>
2023-09-13 12:32:21 +0200

    Add secret_key_base to config

--- apps/api/config/runtime.exs
+++ apps/api/config/runtime.exs
@@ -30,6 +30,8 @@ if config_env() == :prod do
   host = System.get_env("PHX_HOST") || "example.com"
   port = String.to_integer(System.get_env("PORT") || "4000")
 
+  config :buildel, :secret_key_base, secret_key_base
+
   config :buildel, BuildelWeb.Endpoint,
     url: [host: host, port: 443, scheme: "https"],
     http: [
