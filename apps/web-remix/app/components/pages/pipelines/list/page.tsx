8f3cae89d625cedce991f5887490ec11d2fa7367
kielbasa-elp <dawid.kielbasa@elpassion.pl>
2023-09-18 13:52:36 +0200

    do not show list if empty

--- apps/web-remix/app/components/pages/pipelines/list/page.tsx
+++ apps/web-remix/app/components/pages/pipelines/list/page.tsx
@@ -23,18 +23,21 @@ export function PipelinesPage() {
         <Outlet />
       </CreatePipelineModal>
 
-      <div className="px-4 md:px-10 flex items-start gap-8">
-        <div className="max-w-[80rem] flex-grow">
-          <Link
-            to={`/${organizationId}/pipelines/new`}
-            className="mb-6 block w-fit ml-auto mr-0"
-          >
-            <Button size="sm">New Workflow</Button>
-          </Link>
+      <div className="px-4 md:px-10 flex items-start justify-center gap-8">
+        {pipelines.data.length > 0 ? (
+          <div className="max-w-[80rem] flex-grow">
+            <Link
+              to={`/${organizationId}/pipelines/new`}
+              className="mb-6 block w-fit ml-auto mr-0"
+            >
+              <Button size="sm">New Workflow</Button>
+            </Link>
 
-          <PipelinesList pipelines={pipelines.data} />
-        </div>
-        <div className="flex-grow max-w-[25rem] text-white">
+            <PipelinesList pipelines={pipelines.data} />
+          </div>
+        ) : null}
+
+        <div className="flex-grow max-w-[25rem] bg-neutral-800 p-2 text-white h-[500px] rounded-lg">
           Tempaltes here...
         </div>
       </div>
