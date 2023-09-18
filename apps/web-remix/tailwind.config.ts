0dc26dfb386ea350fb2d79c9a8d52c56da3d2850
kielbasa-elp <dawid.kielbasa@elpassion.pl>
2023-09-18 11:34:23 +0200

    Setup custom figma tokens for button and iconButton

--- apps/web-remix/tailwind.config.ts
+++ apps/web-remix/tailwind.config.ts
@@ -1,8 +1,31 @@
 import { join } from "path";
-import { initDefaultTheme } from "@elpassion/taco";
-import { merge } from "lodash";
+import { initDefaultTheme, colorsTokenHelpers } from "@elpassion/taco";
+import merge from "lodash.merge";
+import colorsJson from "./tokens/colors.json";
+const { getColorsConfig, getComponentsColorConfig } = colorsTokenHelpers;
 
-console.log(__dirname);
+const palette = getColorsConfig(colorsJson);
+
+const importJson = (component: string) =>
+  require(`./tokens/components/${component}.json`);
+
+const customConfig = {
+  extend: {
+    ...merge(
+      //@ts-ignore
+      ...getComponentsColorConfig(
+        ["button", ["iconButton"]],
+        importJson,
+        colorsJson
+      ),
+      {
+        colors: {
+          ...palette,
+        },
+      }
+    ),
+  },
+};
 
 export default {
   darkMode: "class",
@@ -11,6 +34,6 @@ export default {
     "./app/**/*.{js,ts,jsx,tsx,mdx}",
     join(__dirname, "./node_modules/@elpassion/taco/**/*.{js,jsx}"),
   ],
-  theme: merge({}, initDefaultTheme(), {}),
+  theme: merge({}, initDefaultTheme(), customConfig),
   plugins: [require("@tailwindcss/forms")],
 };
