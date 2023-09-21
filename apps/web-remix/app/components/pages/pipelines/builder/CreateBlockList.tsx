c57a3edd3f541ba1093866ecf309de139c9729a6
Michał Warda <michal.warda@elpassion.pl>
2023-09-21 18:23:31 +0200

    Add grouped inputs

--- apps/web-remix/app/components/pages/pipelines/builder/CreateBlockList.tsx
+++ apps/web-remix/app/components/pages/pipelines/builder/CreateBlockList.tsx
@@ -1,6 +1,6 @@
 import { z } from "zod";
 import { useReactFlow, useViewport } from "reactflow";
-import React, { DragEvent, useCallback } from "react";
+import React, { DragEvent, useCallback, useMemo } from "react";
 import startCase from "lodash.startcase";
 import classNames from "classnames";
 import { useRunPipeline } from "./RunPipelineProvider";
@@ -43,33 +43,56 @@ export const CreateBlockList: React.FC<CreateBlockListProps> = ({
     [onCreate, reactFlowInstance]
   );
 
+  const blockGroups = useMemo(
+    () =>
+      blockTypes.reduce((groups, blockType) => {
+        blockType.groups.forEach((group) => {
+          if (!groups[group]) {
+            groups[group] = [] as IBlockTypes;
+          }
+          groups[group].push(blockType);
+        });
+
+        return groups;
+      }, {} as Record<string, IBlockTypes>),
+    [blockTypes]
+  );
+
   return (
-    <ul className="flex flex-col gap-2">
-      {blockTypes.map((block) => (
-        <li
-          key={block.type}
-          className={classNames(
-            "group min-w-[150px] cursor-grab rounded-lg bg-neutral-800 py-2 pl-4 pr-2 text-white flex justify-between items-center",
-            {
-              "opacity-50": runStatus !== "idle",
-            }
-          )}
-          onDragStart={(event) => {
-            if (runStatus !== "idle") return;
-            onDragStart(event, block.type);
-          }}
-          draggable
-        >
-          <span>{startCase(block.type)}</span>
-          <Button
-            className="opacity-0 group-hover:opacity-100"
-            size="xs"
-            onClick={() => onClickAdd(block)}
-          >
-            Add
-          </Button>
-        </li>
+    <div className="space-y-3 overflow-y-auto">
+      {Object.entries(blockGroups).map(([group, blocks]) => (
+        <div>
+          <p className="text-xs text-white">{startCase(group)}</p>
+
+          <ul className="flex flex-col gap-2 mt-2">
+            {blocks.map((block) => (
+              <li
+                key={block.type}
+                className={classNames(
+                  "group min-w-[150px] cursor-grab rounded-lg bg-neutral-800 py-2 pl-4 pr-2 text-white flex justify-between items-center",
+                  {
+                    "opacity-50": runStatus !== "idle",
+                  }
+                )}
+                onDragStart={(event) => {
+                  if (runStatus !== "idle") return;
+                  onDragStart(event, block.type);
+                }}
+                draggable
+              >
+                <span>{startCase(block.type)}</span>
+                <Button
+                  className="opacity-0 group-hover:opacity-100"
+                  size="xs"
+                  onClick={() => onClickAdd(block)}
+                >
+                  Add
+                </Button>
+              </li>
+            ))}
+          </ul>
+        </div>
       ))}
-    </ul>
+    </div>
   );
 };
