d4118372ebd6aeea6eefab8698b93e1f259b80f8
Michał Warda <michal.warda@elpassion.pl>
2023-09-25 13:51:53 +0200

    Add advanced caching to comparison servings!

--- apps/api/lib/buildel/hybrid_db.ex
+++ apps/api/lib/buildel/hybrid_db.ex
@@ -2,7 +2,11 @@ defmodule Buildel.HybridDB do
   require Logger
   use Buildel.Utils.TelemetryWrapper
 
+  @batch_size 16
+
   deftimed query(collection_name, query), [:buildel, :hybrid_db, :query] do
+    query = query |> String.trim() |> String.downcase()
+
     search_results = Buildel.SearchDB.query(collection_name, query)
 
     vector_results = Buildel.VectorDB.query(collection_name, query, api_key: api_key())
@@ -12,14 +16,42 @@ defmodule Buildel.HybridDB do
   end
 
   deftimedp sort_results_by_query(results, query), [:buildel, :hybrid_db, :sorting] do
-    sorted_results =
-      Nx.Serving.batched_run(
-        __MODULE__,
-        results |> Enum.map(fn result -> {result["document"], query} end)
-      )
-      |> Map.get(:logits)
-      |> Nx.to_list()
+    cached_results =
+      results
+      |> Enum.map(fn result -> "sortings::#{result["metadata"]["chunk_id"]}::#{query}" end)
+      |> Enum.map(&Buildel.DocumentCache.get/1)
+
+    missing_results =
+      cached_results
       |> Enum.zip(results)
+      |> Enum.filter(fn
+        {nil, _} -> true
+        {_cached_result, _result} -> false
+      end)
+      |> Enum.map(fn {_cached_result, result} -> result end)
+
+    result_tuples = missing_results |> Enum.map(fn result -> {result["document"], query} end)
+
+    processed_missing_results =
+      if Enum.empty?(missing_results) do
+        []
+      else
+        Nx.Serving.batched_run(__MODULE__, result_tuples)
+        |> Map.get(:logits)
+        |> Nx.to_list()
+        |> Enum.zip(missing_results)
+        |> Enum.map(fn {_, document} = result ->
+          Buildel.DocumentCache.put(
+            "sortings::#{document["metadata"]["chunk_id"]}::#{query}",
+            result
+          )
+        end)
+      end
+
+    processed_cached_results = cached_results |> Enum.filter(&(&1 != nil))
+
+    sorted_results =
+      (processed_cached_results ++ processed_missing_results)
       |> Enum.sort_by(fn {[score], _result} -> score end, &>=/2)
       |> Enum.map(fn {_score, result} -> result end)
 
@@ -38,26 +70,22 @@ defmodule Buildel.HybridDB do
   def serving() do
     {:ok, model_info} = Bumblebee.load_model({:hf, "cross-encoder/ms-marco-TinyBERT-L-2-v2"})
     {:ok, tokenizer} = Bumblebee.load_tokenizer({:hf, "bert-base-uncased"})
-    batch_size = 4
 
-    Nx.Serving.new(
-      fn _ ->
-        {_init_fn, predict_fn} = Axon.build(model_info.model, compiler: EXLA)
+    Nx.Serving.new(fn _ ->
+      {_init_fn, predict_fn} = Axon.build(model_info.model, compiler: EXLA)
 
-        fn %{size: size} = inputs ->
-          inputs = Nx.Batch.pad(inputs, batch_size - size)
-          {time, prediction_result} = :timer.tc(fn -> predict_fn.(model_info.params, inputs) end)
+      fn %{size: size} = inputs ->
+        inputs = Nx.Batch.pad(inputs, @batch_size - size)
+        {time, prediction_result} = :timer.tc(fn -> predict_fn.(model_info.params, inputs) end)
 
-          :telemetry.execute(
-            [:buildel, :hybrid_db, :sorting, :batch_processing],
-            %{duration: time * 1000}
-          )
+        :telemetry.execute(
+          [:buildel, :hybrid_db, :sorting, :batch_processing],
+          %{duration: time * 1000}
+        )
 
-          prediction_result
-        end
-      end,
-      batch_size: batch_size
-    )
+        prediction_result
+      end
+    end)
     |> Nx.Serving.client_preprocessing(fn input ->
       inputs = Bumblebee.apply_tokenizer(tokenizer, input)
 
