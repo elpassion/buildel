89d4e9f8b34f600484b81baab31029e633fbefb4
kielbasa-elp <dawid.kielbasa@elpassion.pl>
2023-09-25 16:16:02 +0200

    Add confirmation fnc

---
+++ apps/web-remix/app/components/modal/ConfirmationModal.tsx
@@ -0,0 +1,77 @@
+import React from "react";
+import { Modal } from "@elpassion/taco/Modal";
+import { Button, Icon, ModalProps } from "@elpassion/taco";
+import { IconButton } from "~/components/iconButton";
+import classNames from "classnames";
+export interface ConfirmationModalProps extends ModalProps {
+  onConfirm?: () => Promise<void>;
+  onCancel?: () => Promise<void>;
+  confirmText?: React.ReactNode;
+  cancelText?: React.ReactNode;
+}
+
+export const ConfirmationModal: React.FC<ConfirmationModalProps> = ({
+  children,
+  className,
+  onConfirm,
+  onCancel,
+  cancelText = "Cancel",
+  confirmText = "Confirm",
+  ...props
+}) => {
+  const handleConfirm = async () => {
+    await onConfirm?.();
+    props.onClose();
+  };
+  const handleCancel = async () => {
+    await onCancel?.();
+    props.onClose();
+  };
+
+  return (
+    <Modal
+      header={
+        props.header ?? <ConfirmationModalHeader onClose={props.onClose} />
+      }
+      className={classNames("max-w-[500px] min-w-[300px] mx-2", className)}
+      {...props}
+    >
+      {children}
+
+      <div className="flex gap-2 justify-end">
+        <Button size="md" type="button" variant="filled" onClick={handleCancel}>
+          {cancelText}
+        </Button>
+        <Button
+          size="md"
+          type="submit"
+          variant="ghost"
+          hierarchy="destructive"
+          onClick={handleConfirm}
+        >
+          {confirmText}
+        </Button>
+      </div>
+    </Modal>
+  );
+};
+
+interface ConfirmationModalHeaderProps {
+  onClose: () => void;
+}
+
+export function ConfirmationModalHeader({
+  onClose,
+}: ConfirmationModalHeaderProps) {
+  return (
+    <header className="flex justify-between gap-2 items-center">
+      <h3 className="text-white font-medium text-xl">Are you sure?</h3>
+      <IconButton
+        onClick={onClose}
+        variant="outlined"
+        size="sm"
+        icon={<Icon iconName="x" />}
+      />
+    </header>
+  );
+}
---
+++ apps/web-remix/app/components/modal/confirm.tsx
@@ -0,0 +1,40 @@
+import * as ReactDom from "react-dom/client";
+import { ConfirmationModal, ConfirmationModalProps } from "./ConfirmationModal";
+import React from "react";
+
+export type ConfirmProps = Omit<ConfirmationModalProps, "isOpen" | "onClose">;
+
+export function confirm(config: ConfirmProps) {
+  let destroyFn: (() => void) | null = null;
+  const container = document.createDocumentFragment();
+  const currentConfig = {
+    ...config,
+    onClose: close,
+    isOpen: true,
+  };
+
+  function render() {
+    const root = domRender(<ConfirmationModal {...currentConfig} />, container);
+
+    destroyFn = () => root.unmount();
+  }
+
+  function close() {
+    if (typeof destroyFn === "function") {
+      destroyFn();
+    }
+  }
+
+  render();
+}
+
+function domRender(
+  element: React.ReactNode,
+  container: DocumentFragment | Element
+) {
+  const root = ReactDom.createRoot(container);
+
+  root.render(element);
+
+  return root;
+}
