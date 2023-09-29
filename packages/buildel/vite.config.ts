3454638605d4d85500076bdb03e07f501e1bf02b
kielbasa-elp <dawid.kielbasa@elpassion.pl>
2023-09-29 10:02:16 +0200

    Add ts-utils to optimizeDeps

--- packages/buildel/vite.config.ts
+++ packages/buildel/vite.config.ts
@@ -4,6 +4,9 @@ import dts from "vite-plugin-dts";
 import tsconfigPaths from "vite-tsconfig-paths";
 // https://vitejs.dev/config/
 export default defineConfig({
+  optimizeDeps: {
+    include: ["ts-utils"],
+  },
   build: {
     lib: {
       entry: resolve(__dirname, "src/index.ts"),
