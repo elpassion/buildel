8447dc20b14e9367e2f7d147ab3978e64ab32bbf
kielbasa-elp <dawid.kielbasa@elpassion.pl>
2023-10-03 13:34:41 +0200

    Style select a bit

--- apps/web-remix/app/components/form/inputs/asyncSelect.input.tsx
+++ apps/web-remix/app/components/form/inputs/asyncSelect.input.tsx
@@ -1,4 +1,23 @@
 import { AsyncSelect, IAsyncSelectProps } from "@elpassion/taco/Dropdown";
 
 export type AsyncSelectInputProps = IAsyncSelectProps;
-export const AsyncSelectInput = AsyncSelect;
+
+export const AsyncSelectInput: React.FC<AsyncSelectInputProps> = (props) => {
+  return (
+    <AsyncSelect
+      customStyles={{
+        control: {
+          "background-color": "#454545 !important",
+        },
+        menu: {
+          "background-color": "#454545 !important",
+          overflow: "hidden",
+        },
+        option: {
+          "background-color": "#454545 !important",
+        },
+      }}
+      {...props}
+    />
+  );
+};
