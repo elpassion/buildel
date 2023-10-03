d8dda7ba52ee78ad80e2f7a599ba30e1b3281466
kielbasa-elp <dawid.kielbasa@elpassion.pl>
2023-10-03 12:55:55 +0200

    Remove unused fields

--- apps/web-remix/app/components/form/inputs/radioGroup.input.tsx
+++
@@ -1,4 +0,0 @@
-import { RadioCardGroup, RadioCardGroupProps } from "@elpassion/taco";
-
-export type RadioCardGroupInputProps = RadioCardGroupProps;
-export const RadioCardGroupInput = RadioCardGroup;
