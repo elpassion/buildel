2f0688d924cf7f89f5a7ba4d89e7d3ab2f07352c
kielbasa-elp <dawid.kielbasa@elpassion.pl>
2023-10-16 10:48:49 +0200

    Change tooltip styles

--- apps/web-remix/app/components/tooltip/Tooltip.tsx
+++ apps/web-remix/app/components/tooltip/Tooltip.tsx
@@ -1,12 +1,18 @@
 import React from "react";
 import { Tooltip as ReactTooltip, ITooltip } from "react-tooltip";
+import classNames from "classnames";
 
-export const Tooltip: React.FC<ITooltip> = ({ children, ...props }) => {
+export const Tooltip: React.FC<ITooltip> = ({
+  children,
+  className,
+  ...props
+}) => {
   return (
     <ReactTooltip
       place="right"
       openOnClick={false}
-      className="!text-primary-500"
+      className={classNames("!text-primary-500 !py-1 !px-2", className)}
+      offset={17}
       {...props}
     >
       {children}
