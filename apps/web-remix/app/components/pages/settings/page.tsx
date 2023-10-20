d320271a4c9a6f82c660a5d9a8723a3fb0252e5d
kielbasa-elp <dawid.kielbasa@elpassion.pl>
2023-10-20 13:39:19 +0200

    Display layout on settings page

--- apps/web-remix/app/components/pages/settings/page.tsx
+++ apps/web-remix/app/components/pages/settings/page.tsx
@@ -1,10 +1,20 @@
 import { MetaFunction } from "@remix-run/node";
 import { useLoaderData } from "@remix-run/react";
 import { loader } from "./loader";
+import { AppNavbar, AppNavbarHeading } from "~/components/navbar/AppNavbar";
+import React from "react";
+import { PageContentWrapper } from "~/components/layout/PageContentWrapper";
 
 export function SettingsPage() {
   const { user } = useLoaderData<typeof loader>();
-  return <p className="text-white">user: {user.id}</p>;
+  return (
+    <>
+      <AppNavbar leftContent={<AppNavbarHeading>Settings</AppNavbarHeading>} />
+      <PageContentWrapper>
+        <p>user: {user.id}</p>
+      </PageContentWrapper>
+    </>
+  );
 }
 
 export const meta: MetaFunction = () => {
