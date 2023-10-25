abfa8c185f9ea79b488cb1f54fdb27ee8ee23ce3
kielbasa-elp <dawid.kielbasa@elpassion.pl>
2023-10-25 11:36:15 +0200

    wip: different setup ;v

--- apps/web-remix/app/components/form/Form.tsx
+++ apps/web-remix/app/components/form/Form.tsx
@@ -1,7 +1,11 @@
-import React, { PropsWithChildren } from "react";
-import { Form as RemixForm } from "@remix-run/react";
-import { useForm } from "@conform-to/react";
+import React, { forwardRef, PropsWithChildren } from "react";
+import {
+  Form as RemixForm,
+  FormProps as RemixFormProps,
+} from "@remix-run/react";
+import { conform, useForm } from "@conform-to/react";
 import { FormConfig } from "@conform-to/react/hooks";
+import { TextInput, TextInputProps } from "~/components/form/inputs/text.input";
 
 interface IFormContext {
   fields: Record<string, any>;
@@ -11,13 +15,16 @@ const FormContext = React.createContext<IFormContext | null>(null);
 
 interface FormProps<T extends Record<string, any>>
   extends PropsWithChildren,
+    Omit<RemixFormProps, "onSubmit" | "defaultValue">,
     FormConfig<T, T> {}
 export function Form<T extends Record<string, any>>({
   children,
   ...config
 }: FormProps<T>) {
-  const [form, fields] = useForm<T>(config);
-
+  const [form, fields] = useForm<T>({
+    shouldValidate: "onSubmit",
+    ...config,
+  });
   return (
     <FormContext.Provider value={{ fields }}>
       <RemixForm {...form.props}>{children}</RemixForm>
@@ -25,15 +32,42 @@ export function Form<T extends Record<string, any>>({
   );
 }
 
-export function useField({ name }: { name: string }) {
+interface BaseField {
+  error?: string;
+  errors?: string[];
+  name: string;
+  defaultValue?: any;
+}
+
+export function useField({ name }: { name: string }): BaseField {
   const ctx = React.useContext(FormContext);
 
   if (!ctx) {
     throw new Error("You tried to use a field without using <Form />");
   }
-  if (!Object.keys(ctx).includes(name)) {
-    throw new Error("Wrong Field Name");
+
+  const field = ctx.fields[name];
+
+  if (!field) {
+    throw new Error(`There is no ${name} field in the form`);
   }
 
-  return ctx.fields[name];
+  return field;
 }
+
+export const SampleTextInputField = forwardRef<
+  HTMLInputElement,
+  Partial<TextInputProps>
+>((props, ref) => {
+  const field = useField({ name: props.name! });
+
+  return (
+    <TextInput
+      {...conform.input(field, {
+        type: "text",
+      })}
+      errorMessage={field.error}
+      {...props}
+    />
+  );
+});
--- apps/web-remix/app/components/pages/settings/page.tsx
+++ apps/web-remix/app/components/pages/settings/page.tsx
@@ -1,9 +1,34 @@
-import { MetaFunction } from "@remix-run/node";
+import { ActionFunctionArgs, json, MetaFunction } from "@remix-run/node";
 import { useLoaderData } from "@remix-run/react";
 import { loader } from "./loader";
 import { AppNavbar, AppNavbarHeading } from "~/components/navbar/AppNavbar";
 import React from "react";
 import { PageContentWrapper } from "~/components/layout/PageContentWrapper";
+import { Form, SampleTextInputField } from "~/components/form/Form";
+import { parse } from "@conform-to/zod";
+import z from "zod";
+import { Button } from "@elpassion/taco";
+import { actionBuilder } from "~/utils.server";
+import { requireLogin } from "~/session.server";
+import invariant from "tiny-invariant";
+
+const schema = z.object({
+  email: z.string().email(),
+});
+export async function action(actionArgs: ActionFunctionArgs) {
+  return actionBuilder({
+    post: async ({ params, request }, { fetch }) => {
+      await requireLogin(request);
+      invariant(params.organizationId, "Missing organizationId");
+      const formData = await request.formData();
+      const result = parse(formData, { schema });
+
+      console.log(result);
+
+      return json({});
+    },
+  })(actionArgs);
+}
 
 export function SettingsPage() {
   const { user } = useLoaderData<typeof loader>();
@@ -11,7 +36,21 @@ export function SettingsPage() {
     <>
       <AppNavbar leftContent={<AppNavbarHeading>Settings</AppNavbarHeading>} />
       <PageContentWrapper>
-        <p>user: {user.id}</p>
+        <Form
+          onValidate={({ formData }) => {
+            return parse(formData, {
+              schema,
+            });
+          }}
+          method="post"
+        >
+          <SampleTextInputField
+            name="email"
+            label="email"
+            supportingText="Your email"
+          />
+          <Button type="submit">submit</Button>
+        </Form>
       </PageContentWrapper>
     </>
   );
