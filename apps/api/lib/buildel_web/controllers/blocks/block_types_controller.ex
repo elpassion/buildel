89b67faef937c453d2b71aca850f7c04e87a9a62
kielbasa-elp <dawid.kielbasa@elpassion.pl>
2024-01-23 16:50:36 +0100

    Handle fetching single block_type

--- apps/api/lib/buildel_web/controllers/blocks/block_types_controller.ex
+++ apps/api/lib/buildel_web/controllers/blocks/block_types_controller.ex
@@ -1,7 +1,11 @@
 defmodule BuildelWeb.BlockTypesController do
   use Phoenix.Controller
 
-  def index(conn, _params) do
+  def index(conn, %{"block_name" => block_name}) do
+    render(conn, :index, block: Buildel.Blocks.type(block_name).options)
+  end
+
+  def index(conn,  _params) do
     render(conn, :index, block_types: Buildel.Blocks.list_types())
   end
 end
--- apps/api/lib/buildel_web/controllers/blocks/block_types_view.ex
+++ apps/api/lib/buildel_web/controllers/blocks/block_types_view.ex
@@ -2,4 +2,8 @@ defmodule BuildelWeb.BlockTypesView do
   def index(%{block_types: block_types}) do
     %{data: block_types}
   end
+
+  def index(%{block: block}) do
+    %{data: block}
+  end
 end
