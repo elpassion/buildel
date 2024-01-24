9fde73270d4d1c3b26daf153fc2c9a68633efad0
kielbasa-elp <dawid.kielbasa@elpassion.pl>
2024-01-24 10:08:59 +0100

    Change block_types params

--- apps/api/lib/buildel_web/controllers/blocks/block_types_controller.ex
+++ apps/api/lib/buildel_web/controllers/blocks/block_types_controller.ex
@@ -1,8 +1,8 @@
 defmodule BuildelWeb.BlockTypesController do
   use Phoenix.Controller
 
-  def index(conn, %{"block_name" => block_name}) do
-    render(conn, :index, block: Buildel.Blocks.type(block_name).options)
+  def index(conn, %{"type" => type}) do
+    render(conn, :index, block_types: Buildel.Blocks.list_types() |> Enum.filter(fn block -> block.type == type end))
   end
 
   def index(conn,  _params) do
--- apps/api/lib/buildel_web/controllers/blocks/block_types_view.ex
+++ apps/api/lib/buildel_web/controllers/blocks/block_types_view.ex
@@ -3,8 +3,8 @@ defmodule BuildelWeb.BlockTypesView do
     %{data: block_types}
   end
 
-  def index(%{block: block}) do
-    %{data: block}
+  def index(%{block_types: block_types}) do
+    %{data: block_types}
   end
 
   def overviews(%{block_overviews: block_overviews}) do
