8add4d1d429af0d80a97dcd48655b853f21d2b43
Michal Warda <mw@grifel.dev>
2024-01-26 11:51:12 +0100

    Remove debuggers

--- apps/api/lib/buildel/blocks/create_block_tool.ex
+++ apps/api/lib/buildel/blocks/create_block_tool.ex
@@ -1,15 +1,15 @@
 defmodule Buildel.Blocks.CreateBlockTool do
-  alias LangChain.FunctionParam
   alias Buildel.Pipelines
   alias Buildel.Pipelines.Pipeline
   use Buildel.Blocks.Block
   alias LangChain.Function
   use BuildelWeb.Validator
+  use Buildel.Blocks.Utils.TakeLatest
 
   # Config
 
   @impl true
-  defdelegate cast(pid, chunk), to: __MODULE__, as: :create
+  defdelegate cast(pid, config), to: __MODULE__, as: :create
 
   @impl true
   def options() do
@@ -17,7 +17,10 @@ defmodule Buildel.Blocks.CreateBlockTool do
       type: "create_block_tool",
       description: "Used to create block in workflow abc",
       groups: ["el", "tools"],
-      inputs: [],
+      inputs: [
+        Block.text_input("organization_id"),
+        Block.text_input("pipeline_id")
+      ],
       outputs: [],
       ios: [Block.io("tool", "worker")],
       schema: schema()
@@ -57,7 +60,7 @@ defmodule Buildel.Blocks.CreateBlockTool do
   def init(%{context_id: context_id, type: __MODULE__, opts: opts} = state) do
     subscribe_to_connections(context_id, state.connections)
 
-    {:ok, state |> assign_stream_state(opts)}
+    {:ok, state |> assign_stream_state(opts) |> assign_take_latest()}
   end
 
   @impl true
@@ -85,19 +88,26 @@ defmodule Buildel.Blocks.CreateBlockTool do
   def handle_call({:create_block, arguments}, _caller, state) do
     state = state |> send_stream_start()
 
-    with {:ok, %{block: block_config}} <-
+    with {:ok, organization_id} <- get_input_value(state, "organization_id"),
+         {:ok, pipeline_id} <- get_input_value(state, "pipeline_id"),
+         {:ok, %{block: block_config}} <-
            validate(:create_block, arguments),
-         organization <- Buildel.Organizations.get_organization!("4"),
+         organization <-
+           Buildel.Organizations.get_organization!(organization_id),
          {:ok, %Pipeline{} = pipeline} <-
-           Pipelines.get_organization_pipeline(organization, "31"),
+           Pipelines.get_organization_pipeline(organization, pipeline_id),
          {:ok, %Pipeline{} = _pipeline} <-
            Pipelines.create_block(
              pipeline,
              Map.merge(block_config, %{connections: [], inputs: []})
            ) do
+      state = state |> send_stream_stop()
       {:reply, "Created block", state}
     else
-      _ -> {:reply, "Something went wrong.", state}
+      _ ->
+        state = state |> send_stream_stop()
+
+        {:reply, "Something went wrong.", state}
     end
   end
 
@@ -145,8 +155,8 @@ defmodule Buildel.Blocks.CreateBlockTool do
   end
 
   @impl true
-  def handle_info({_name, :text, text}, state) do
-    cast(self(), {:text, text})
+  def handle_info({name, :text, message}, state) do
+    state = save_latest_input_value(state, name, message)
     {:noreply, state}
   end
 end
