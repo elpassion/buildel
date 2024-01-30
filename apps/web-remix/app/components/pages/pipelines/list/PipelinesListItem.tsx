39a34f2b5ddc86e80e278674ac4614b0b4e164de
kielbasa-elp <dawid.kielbasa@elpassion.pl>
2024-01-30 12:00:50 +0100

    Add duplicate button

--- apps/web-remix/app/components/pages/pipelines/list/PipelinesListItem.tsx
+++ apps/web-remix/app/components/pages/pipelines/list/PipelinesListItem.tsx
@@ -1,10 +1,15 @@
 import { Icon } from "@elpassion/taco";
 import { IPipeline } from "./pipelines.types";
-import React, { PropsWithChildren } from "react";
+import React, { PropsWithChildren, useMemo } from "react";
 import classNames from "classnames";
 import { IconButton } from "~/components/iconButton";
 import { confirm } from "~/components/modal/confirm";
 import { useFetcher } from "@remix-run/react";
+import { routes } from "~/utils/routes.utils";
+import { ValidatedForm } from "remix-validated-form";
+import { HiddenField } from "~/components/form/fields/field.context";
+import { withZod } from "@remix-validated-form/with-zod";
+import { schema } from "./schema";
 
 interface PipelinesListItemProps extends PropsWithChildren {
   className?: string;
@@ -32,6 +37,7 @@ export const PipelineListItemHeader = ({
   pipeline,
 }: PipelineListItemHeaderProps) => {
   const fetcher = useFetcher();
+
   const handleDelete = async (e: React.MouseEvent<HTMLButtonElement>) => {
     e.preventDefault();
     confirm({
@@ -46,6 +52,7 @@ export const PipelineListItemHeader = ({
       ),
     });
   };
+
   return (
     <header className="flex items-start">
       <h2 className="flex basis-1/2 text-lg font-medium">{pipeline.name}</h2>
@@ -53,16 +60,59 @@ export const PipelineListItemHeader = ({
       <div className="flex items-center basis-1/2 justify-between">
         <p className="text-sm">{pipeline.runs_count} runs</p>
 
-        <IconButton
-          size="xs"
-          variant="ghost"
-          aria-label="Remove workflow"
-          className="opacity-0 group-hover:opacity-100 !bg-neutral-700 !text-white !text-sm hover:!text-red-500"
-          title={`Remove workflow: ${pipeline.name}`}
-          icon={<Icon iconName="trash" />}
-          onClick={handleDelete}
-        />
+        <div
+          className="flex gap-1"
+          onClick={(e) => {
+            e.stopPropagation();
+          }}
+        >
+          <DuplicateForm pipeline={pipeline} />
+
+          <IconButton
+            size="xs"
+            variant="ghost"
+            aria-label="Remove workflow"
+            className="opacity-0 group-hover:opacity-100 !bg-neutral-700 !text-white !text-sm hover:!text-red-500"
+            title={`Remove workflow: ${pipeline.name}`}
+            icon={<Icon iconName="trash" />}
+            onClick={handleDelete}
+          />
+        </div>
       </div>
     </header>
   );
 };
+
+interface DuplicateFormProps {
+  pipeline: IPipeline;
+}
+
+function DuplicateForm({ pipeline }: DuplicateFormProps) {
+  const validator = useMemo(() => withZod(schema), []);
+
+  return (
+    <ValidatedForm
+      method="POST"
+      validator={validator}
+      action={routes.pipelinesNew(pipeline.organization_id)}
+    >
+      <HiddenField name="pipeline.name" value={pipeline.name + " copy"} />
+
+      <HiddenField name="pipeline.config.version" value="1" />
+
+      <HiddenField
+        name="pipeline.config.blocks"
+        value={JSON.stringify(pipeline.config.blocks)}
+      />
+
+      <IconButton
+        size="xs"
+        variant="ghost"
+        aria-label="Duplicate workflow"
+        className="opacity-0 group-hover:opacity-100 !bg-neutral-700 !text-white !text-sm hover:!text-primary-500"
+        title={`Duplicate workflow: ${pipeline.name}`}
+        icon={<Icon iconName="copy" />}
+      />
+    </ValidatedForm>
+  );
+}
