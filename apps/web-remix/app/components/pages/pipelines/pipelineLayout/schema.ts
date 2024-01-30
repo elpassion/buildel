3b4c46560c38fa195bff3a57b34382a64d708c82
kielbasa-elp <dawid.kielbasa@elpassion.pl>
2024-01-30 09:15:20 +0100

    Fix update schema

--- apps/web-remix/app/components/pages/pipelines/pipelineLayout/schema.ts
+++ apps/web-remix/app/components/pages/pipelines/pipelineLayout/schema.ts
@@ -4,7 +4,7 @@ import { InterfaceConfig, UpdateBlockConfig } from "../contracts";
 export const updateSchema = z.object({
   id: z.number(),
   name: z.string(),
-  interface_config: InterfaceConfig.optional(),
+  interface_config: z.union([InterfaceConfig, z.null()]),
   config: z.object({
     version: z.string(),
     blocks: z.array(UpdateBlockConfig),
