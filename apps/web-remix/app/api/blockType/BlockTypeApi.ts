bcc9dd7c32ff8f2f7af1744fb3f3b7dc93b66370
Michal Warda <mw@grifel.dev>
2024-02-09 18:30:46 +0100

    Cache block_Types

--- apps/web-remix/app/api/blockType/BlockTypeApi.ts
+++ apps/web-remix/app/api/blockType/BlockTypeApi.ts
@@ -1,10 +1,16 @@
 import { fetchTyped } from "~/utils/fetch.server";
 import { BlockTypesResponse } from "./blockType.contracts";
 
+let cache = undefined as any;
+
 export class BlockTypeApi {
   constructor(private client: typeof fetchTyped) {}
 
-  getBlockTypes() {
-    return this.client(BlockTypesResponse, `/block_types`);
+  async getBlockTypes() {
+    // TODO: Setup better caching
+    if (cache) return cache;
+    const response = await this.client(BlockTypesResponse, `/block_types`);
+    cache = response;
+    return response;
   }
 }
