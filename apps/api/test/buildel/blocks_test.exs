f4cc77d5828bdd7c156897b37726b9a2080128bc
Michal Warda <mw@grifel.dev>
2024-02-10 18:52:55 +0100

    Move text input test to separate file

--- apps/api/test/buildel/blocks_test.exs
+++ apps/api/test/buildel/blocks_test.exs
@@ -1,13 +1,11 @@
 defmodule Buildel.BlocksTest do
   alias Buildel.Blocks
-  alias Buildel.Blocks.TextOutput
   alias Buildel.BlockPubSub
   alias Buildel.BlocksTestRunner
   use Buildel.DataCase
 
   alias Buildel.Blocks.{
     AudioInput,
-    TextInput,
     SpeechToText,
     FileSpeechToText,
     FileSpeechToText,
@@ -21,50 +19,6 @@ defmodule Buildel.BlocksTest do
     CollectAllText,
     TakeLatest
   }
-
-  describe "TextInput" do
-    test "exposes options" do
-      assert TextInput.options() == %{
-               type: "text_input",
-               description:
-                 "This module is crafted for the seamless intake and transmission of textual data.",
-               inputs: [Block.text_input("input", true)],
-               outputs: [Block.text_output("output")],
-               schema: TextInput.schema(),
-               groups: ["text", "inputs / outputs"],
-               ios: []
-             }
-    end
-
-    test "validates schema correctly" do
-      assert :ok =
-               Blocks.validate_block(TextInput, %{"name" => "test", "opts" => %{}, "inputs" => []})
-
-      assert {:error, _} = Blocks.validate_block(TextInput, %{})
-    end
-
-    test "broadcasts text" do
-      {:ok, test_run} =
-        BlocksTestRunner.start_run(%{
-          blocks: [
-            TextInput.create(%{
-              name: "test",
-              opts: %{},
-              connections: [
-                Blocks.Connection.from_connection_string("test:input->input", "text")
-              ]
-            })
-          ]
-        })
-
-      {:ok, topic} = test_run |> BlocksTestRunner.Run.subscribe_to_output("test", "output")
-      text = "text"
-      test_run |> BlocksTestRunner.Run.input("test", "input", {:text, text})
-      assert_receive {^topic, :start_stream, nil}
-      assert_receive {^topic, :text, ^text}
-      assert_receive {^topic, :stop_stream, nil}
-    end
-  end
 
   describe "AudioInput" do
     test "exposes options" do
