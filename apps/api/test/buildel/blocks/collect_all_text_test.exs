24e1db7ef66ab3f5b23e38ee7977686b77c0a0b2
Michal Warda <mw@grifel.dev>
2024-02-10 19:14:37 +0100

    Add CollectAllTextTest and remove blocks_test.exs

---
+++ apps/api/test/buildel/blocks/collect_all_text_test.exs
@@ -0,0 +1,28 @@
+defmodule Buildel.Blocks.CollectAllTextTest do
+  use Buildel.BlockCase
+  alias Blocks.CollectAllText
+
+  test "exposes options" do
+    assert CollectAllText.options() == %{
+             type: "collect_all_text",
+             description:
+               "This module specializes in accumulating and consolidating text input from streaming sources.",
+             inputs: [Block.text_input("input")],
+             outputs: [Block.text_output("output")],
+             schema: CollectAllText.schema(),
+             groups: ["text", "utils"],
+             ios: []
+           }
+  end
+
+  test "validates schema correctly" do
+    assert :ok =
+             Blocks.validate_block(CollectAllText, %{
+               "name" => "test",
+               "opts" => %{},
+               "inputs" => []
+             })
+
+    assert {:error, _} = Blocks.validate_block(CollectAllText, %{})
+  end
+end
