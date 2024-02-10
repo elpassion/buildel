9339102b4c1891d589ac11b8aa513fcc9a2a2413
Michal Warda <mw@grifel.dev>
2024-02-10 21:41:16 +0100

    Add missing organization_controller specs

--- apps/api/test/buildel_web/controllers/organization_controller_test.exs
+++ apps/api/test/buildel_web/controllers/organization_controller_test.exs
@@ -1,6 +1,7 @@
 defmodule BuildelWeb.OrganizationControllerTest do
   use BuildelWeb.ConnCase
   import Buildel.OrganizationsFixtures
+  alias Buildel.Repo
   alias Buildel.Organizations
 
   setup %{conn: conn} do
@@ -32,6 +33,12 @@ defmodule BuildelWeb.OrganizationControllerTest do
       assert json_response(conn, 401)["errors"] != %{}
     end
 
+    test "does not allow to access another user's organization", %{conn: conn} do
+      organization = organization_fixture()
+      conn = get(conn, ~p"/api/organizations/#{organization.id}")
+      assert json_response(conn, 404)["errors"] != %{}
+    end
+
     test "returns the organization with given id", %{conn: conn, organization: organization} do
       conn = get(conn, ~p"/api/organizations/#{organization.id}")
 
@@ -52,6 +59,13 @@ defmodule BuildelWeb.OrganizationControllerTest do
       assert json_response(conn, 401)["errors"] != %{}
     end
 
+    test "validates organization name", %{conn: conn} do
+      conn =
+        post(conn, ~p"/api/organizations", organization: %{})
+
+      assert json_response(conn, 422)["errors"] != %{}
+    end
+
     test "returns created organization", %{conn: conn} do
       conn =
         post(conn, ~p"/api/organizations", organization: %{name: "some name"})
@@ -70,10 +84,69 @@ defmodule BuildelWeb.OrganizationControllerTest do
       assert json_response(conn, 401)["errors"] != %{}
     end
 
+    test "does not allow to update another user's organization", %{conn: conn} do
+      organization = organization_fixture()
+
+      conn =
+        put(conn, ~p"/api/organizations/#{organization.id}", %{organization: %{name: "new name"}})
+
+      assert json_response(conn, 404)["errors"] != %{}
+    end
+
     test "returns the organization", %{conn: conn, organization: organization} do
       put(conn, ~p"/api/organizations/#{organization.id}", organization: %{name: "new name"})
 
       assert Organizations.get_organization!(organization.id).name == "new name"
+    end
+  end
+
+  describe "get_api_key" do
+    test "requires authentication", %{conn: conn, organization: organization} do
+      conn = conn |> log_out_user()
+
+      conn = get(conn, ~p"/api/organizations/#{organization.id}/api_key")
+
+      assert json_response(conn, 401)["errors"] != %{}
+    end
+
+    test "does not allow to access another user's organization", %{conn: conn} do
+      organization = organization_fixture()
+      conn = get(conn, ~p"/api/organizations/#{organization.id}/api_key")
+      assert json_response(conn, 404)["errors"] != %{}
+    end
+
+    test "returns the organization api key", %{conn: conn, organization: organization} do
+      conn = get(conn, ~p"/api/organizations/#{organization.id}/api_key")
+
+      assert json_response(conn, 200)["data"] == %{
+               "key" => organization.api_key
+             }
+    end
+  end
+
+  describe "reset_api_key" do
+    test "requires authentication", %{conn: conn, organization: organization} do
+      conn = conn |> log_out_user()
+
+      conn = post(conn, ~p"/api/organizations/#{organization.id}/api_key")
+
+      assert json_response(conn, 401)["errors"] != %{}
+    end
+
+    test "does not allow to access another user's organization", %{conn: conn} do
+      organization = organization_fixture()
+      conn = post(conn, ~p"/api/organizations/#{organization.id}/api_key")
+      assert json_response(conn, 404)["errors"] != %{}
+    end
+
+    test "returns the organization with new api key", %{conn: conn, organization: organization} do
+      conn = post(conn, ~p"/api/organizations/#{organization.id}/api_key")
+
+      organization = organization |> Repo.reload!()
+
+      assert json_response(conn, 200)["data"] == %{
+               "key" => organization.api_key
+             }
     end
   end
 
