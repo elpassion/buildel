9890d595889a6dd77359fce10eb88baef7c45e39
kielbasa-elp <dawid.kielbasa@elpassion.pl>
2024-02-11 12:06:44 +0100

    Prepare action/loader with session for tests

--- apps/web-remix/app/tests/setup.tests.ts
+++ apps/web-remix/app/tests/setup.tests.ts
@@ -1,4 +1,10 @@
 import { createRemixStub, RemixStubProps } from "@remix-run/testing";
+import {
+  LoaderFunctionArgs,
+  LoaderFunction,
+  ActionFunction,
+  ActionFunctionArgs,
+} from "@remix-run/node";
 
 type RemixRoutesProps = Parameters<typeof createRemixStub>;
 
@@ -8,3 +14,21 @@ export const setupRoutes = (
 ) => createRemixStub(routes, ctx);
 
 export type RoutesProps = RemixStubProps;
+
+export const getSessionCookie = () => {
+  return "_buildel_key=123";
+};
+
+export const loaderWithSession = (loader: LoaderFunction) => {
+  return (args: LoaderFunctionArgs) => {
+    args.request.headers.set("cookie", getSessionCookie());
+    return loader(args);
+  };
+};
+
+export const actionWithSession = (action: ActionFunction) => {
+  return (args: ActionFunctionArgs) => {
+    args.request.headers.set("cookie", getSessionCookie());
+    return action(args);
+  };
+};
