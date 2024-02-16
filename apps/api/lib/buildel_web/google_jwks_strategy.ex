d6a3f4809d70789fff50c46a1112aac130bde0c8
Michal Warda <mw@grifel.dev>
2024-02-16 15:07:41 +0100

    Fix warning

--- apps/api/lib/buildel_web/google_jwks_strategy.ex
+++ apps/api/lib/buildel_web/google_jwks_strategy.ex
@@ -1,5 +1,5 @@
 defmodule BuildelWeb.GoogleJwksStrategy do
   use JokenJwks.DefaultStrategyTemplate
 
-  def init_opts(opts), do: [jwks_url: "https://www.googleapis.com/oauth2/v3/certs"]
+  def init_opts(_opts), do: [jwks_url: "https://www.googleapis.com/oauth2/v3/certs"]
 end
