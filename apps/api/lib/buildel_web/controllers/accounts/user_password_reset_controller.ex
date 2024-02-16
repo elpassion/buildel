66c865ba940777ea0cb15377cf3c9a110a0f813f
Michal Warda <mw@grifel.dev>
2024-02-16 16:26:07 +0100

    Add reset password email

---
+++ apps/api/lib/buildel_web/controllers/accounts/user_password_reset_controller.ex
@@ -0,0 +1,31 @@
+defmodule BuildelWeb.UserPasswordResetController do
+  use BuildelWeb, :controller
+  use BuildelWeb.Validator
+
+  alias Buildel.Accounts
+
+  action_fallback BuildelWeb.FallbackController
+
+  defparams :create do
+    required(:email, :string, format?: ~r/@/)
+  end
+
+  def create(conn, params) do
+    with {:ok, email} <- validate(:create, params),
+         user <- Accounts.get_user_by_email(email),
+         {:ok, _} <- deliver_user_reset_password_instructions(user) do
+      conn
+      |> put_view(BuildelWeb.UserJSON)
+      |> put_status(:ok)
+      |> json(%{})
+    end
+  end
+
+  defp deliver_user_reset_password_instructions(nil), do: {:ok, nil}
+
+  defp deliver_user_reset_password_instructions(user) do
+    Accounts.deliver_user_reset_password_instructions(user, fn token ->
+      "#{Application.get_env(:buildel, :page_url)}/auth/set_password?token=#{token}"
+    end)
+  end
+end
--- apps/api/lib/buildel_web/router.ex
+++ apps/api/lib/buildel_web/router.ex
@@ -109,6 +109,7 @@ defmodule BuildelWeb.Router do
     post("/users/google/log_in", UserSessionController, :create_google)
     delete("/users/log_out", UserSessionController, :delete)
     put("/users/password", UserPasswordController, :update)
+    post("/users/password/reset", UserPasswordResetController, :reset)
 
     resources("/organizations", OrganizationController, only: [:index, :create, :show])
     put("/organizations/:id", OrganizationController, :update)
