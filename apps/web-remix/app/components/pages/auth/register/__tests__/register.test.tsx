2956d86f6189860a9ae8bb377e909e943101bb65
Michal Warda <t1du53k@gmail.com>
2024-02-19 15:04:01 +0100

    Add web test workflow

--- apps/web-remix/app/components/pages/auth/register/__tests__/register.test.tsx
+++ apps/web-remix/app/components/pages/auth/register/__tests__/register.test.tsx
@@ -6,8 +6,8 @@ import { InputHandle } from "~/tests/handles/Input.handle";
 import { render, screen, waitFor } from "~/tests/render";
 import { server } from "~/tests/server.mock";
 import { RegisterPage } from "../page";
-import { loader } from "../loader";
-import { action } from "../action";
+import { loader } from "../loader.server";
+import { action } from "../action.server";
 import { errorHandlers, handlers } from "./register.handlers";
 
 describe(RegisterPage.name, () => {
