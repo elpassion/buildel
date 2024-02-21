a68ba676952815d0a4b437f6a0489656b82e4f79
kielbasa-elp <dawid.kielbasa@elpassion.pl>
2024-02-21 08:56:33 +0100

    Łaaaaaa bump remix fixed HMR!!!

--- apps/web-remix/vite.config.ts
+++ apps/web-remix/vite.config.ts
@@ -1,4 +1,4 @@
-import { unstable_vitePlugin as remix } from "@remix-run/dev";
+import { vitePlugin as remix } from "@remix-run/dev";
 import { defineConfig } from "vite";
 import { installGlobals } from "@remix-run/node";
 import tsconfigPaths from "vite-tsconfig-paths";
