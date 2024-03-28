96209e87a06dcd6dbf909c9f5877c5575322f843
Michal Warda <t1du53k@gmail.com>
2024-03-28 10:43:32 +0100

    Fix root source code_paths

--- apps/api/config/runtime.exs
+++ apps/api/config/runtime.exs
@@ -83,6 +83,6 @@ if config_env() == :prod do
       dsn: sentry_dsn,
       environment_name: :prod,
       enable_source_code_context: true,
-      root_source_code_paths: File.cwd!()
+      root_source_code_paths: [File.cwd!()]
   end
 end
