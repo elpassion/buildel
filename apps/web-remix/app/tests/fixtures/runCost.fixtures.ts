8511028e5930c88504a7fccce9037259b598efc4
kielbasa-elp <dawid.kielbasa@elpassion.pl>
2024-04-04 15:37:33 +0200

    Fix tests

--- apps/web-remix/app/tests/fixtures/runCost.fixtures.ts
+++ apps/web-remix/app/tests/fixtures/runCost.fixtures.ts
@@ -7,6 +7,8 @@ export const runCostFixture = (
     amount: "0.0000200000",
     created_at: "2024-02-26T14:53:06",
     description: "chat_1",
+    input_tokens: 0,
+    output_tokens: 9,
     id: 1,
     ...override,
   };
