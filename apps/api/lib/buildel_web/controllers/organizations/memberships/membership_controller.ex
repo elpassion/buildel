ff9e1d5c45122ce33d6df0dd58329af3e7fe9845
Michał <mw@grifel.dev>
2024-04-05 12:49:26 +0200

    Handle errors when inviting user

--- apps/api/lib/buildel_web/controllers/organizations/memberships/membership_controller.ex
+++ apps/api/lib/buildel_web/controllers/organizations/memberships/membership_controller.ex
@@ -31,12 +31,46 @@ defmodule BuildelWeb.OrganizationMembershipController do
 
     with {:ok, %{membership: %{user_email: user_email}}} <- validate(:create, params),
          {:ok, organization} <- Organizations.get_user_organization(user, organization_id),
-         user <- Buildel.Accounts.get_user_by_email(user_email),
+         {:ok, user} <- fetch_user_by_email(user_email),
          {:ok, membership} <-
            Organizations.create_membership(%{organization_id: organization.id, user_id: user.id}) do
       conn
       |> put_status(:created)
       |> render(:show, membership: membership)
+    else
+      {:error, :not_found} ->
+        {:error,
+         changeset_for_errors(%{
+           "membership.user_email": "User with specified email does not exist."
+         })}
+
+      {:error, %Ecto.Changeset{errors: [organization_id: {"has already been taken", _}]}} ->
+        {:error,
+         changeset_for_errors(%{
+           "membership.user_email": "User is already a member of the organization."
+         })}
+
+      err ->
+        err
     end
+  end
+
+  defp fetch_user_by_email(email) do
+    case Buildel.Accounts.get_user_by_email(email) do
+      nil -> {:error, :not_found}
+      user -> {:ok, user}
+    end
+  end
+
+  defp changeset_for_errors(errors) do
+    %Ecto.Changeset{
+      action: :validate,
+      errors:
+        errors
+        |> Enum.map(fn {key, value} -> {key, {value, []}} end)
+        |> Enum.into(%{}),
+      changes: %{},
+      types: %{}
+    }
   end
 end
