5f8a2907b01cc1abecd8ef236876ef744f85ec07
kielbasa-elp <dawid.kielbasa@elpassion.pl>
2024-04-09 09:00:19 +0200

    Add search params to redirectTo

--- apps/web-remix/app/components/pages/organizations/invitation/page.tsx
+++ apps/web-remix/app/components/pages/organizations/invitation/page.tsx
@@ -1,11 +1,7 @@
 import { MetaFunction } from "@remix-run/node";
 
 export function InvitationPage() {
-  return (
-    <div className="min-h-screen w-full flex justify-center items-center p-2">
-      Invitation
-    </div>
-  );
+  return null;
 }
 
 export const meta: MetaFunction = () => {
