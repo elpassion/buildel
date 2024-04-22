d1bf0e6c7e6cf90ab05d825d02cb91c3b18128d4
kielbasa-elp <dawid.kielbasa@elpassion.pl>
2024-04-22 08:35:55 +0200

    Added search info at collection interface page

--- apps/web-remix/app/components/pages/knowledgeBase/collectionInterface/loader.server.tsx
+++ apps/web-remix/app/components/pages/knowledgeBase/collectionInterface/loader.server.tsx
@@ -2,6 +2,7 @@ import { json, LoaderFunctionArgs } from "@remix-run/node";
 import invariant from "tiny-invariant";
 import { loaderBuilder } from "~/utils.server";
 import { requireLogin } from "~/session.server";
+import { KnowledgeBaseApi } from "~/api/knowledgeBase/KnowledgeBaseApi";
 
 export async function loader(args: LoaderFunctionArgs) {
   return loaderBuilder(async ({ request, params }, { fetch }) => {
@@ -9,9 +10,20 @@ export async function loader(args: LoaderFunctionArgs) {
     invariant(params.organizationId, "organizationId not found");
     invariant(params.collectionName, "collectionName not found");
 
+    const knowledgeBaseApi = new KnowledgeBaseApi(fetch);
+
+    const {
+      data: { id: collectionId },
+    } = await knowledgeBaseApi.getCollectionByName(
+      params.organizationId,
+      params.collectionName
+    );
+
     return json({
+      collectionId,
       organizationId: params.organizationId,
       collectionName: params.collectionName,
+      apiUrl: process.env.API_URL!,
     });
   })(args);
 }
--- apps/web-remix/app/components/pages/knowledgeBase/collectionInterface/page.tsx
+++ apps/web-remix/app/components/pages/knowledgeBase/collectionInterface/page.tsx
@@ -1,13 +1,28 @@
 import React from "react";
 import classNames from "classnames";
 import { MetaFunction } from "@remix-run/node";
-import { Outlet, useLoaderData, useMatch, useNavigate } from "@remix-run/react";
+import {
+  Link,
+  Outlet,
+  useLoaderData,
+  useMatch,
+  useNavigate,
+} from "@remix-run/react";
 import { ActionSidebar } from "~/components/sidebar/ActionSidebar";
 import { routes } from "~/utils/routes.utils";
 import { loader } from "./loader.server";
+import {
+  InterfaceSectionHeader,
+  InterfaceSectionHeaderParagraph,
+  InterfaceSectionHeading,
+  InterfaceSectionWrapper,
+} from "~/components/interfaces/InterfaceSection";
+import { CodePreviewWrapper } from "~/components/interfaces/CodePreview/CodePreviewWrapper";
+import { CopyCodeButton } from "~/components/actionButtons/CopyCodeButton";
 
 export function KnowledgeBaseCollectionInterface() {
-  const { organizationId, collectionName } = useLoaderData<typeof loader>();
+  const { organizationId, collectionName, collectionId, apiUrl } =
+    useLoaderData<typeof loader>();
   const navigate = useNavigate();
 
   const matchSearch = useMatch(
@@ -21,7 +36,54 @@ export function KnowledgeBaseCollectionInterface() {
 
   return (
     <>
-      <p>Interface</p>
+      <div>
+        <div>
+          <h2 className="text-lg text-white font-medium">HTTP API</h2>
+          <p className="text-white text-xs mb-6 max-w-lg">
+            Access our HTTP API to manage Knowledge Base with capabilities such
+            as searching, adding, modifying, and removing documents.
+          </p>
+        </div>
+
+        <InterfaceSectionWrapper>
+          <InterfaceSectionHeader>
+            <InterfaceSectionHeading>Search</InterfaceSectionHeading>
+            <InterfaceSectionHeaderParagraph>
+              Use this endpoint to perform semantic searches.
+            </InterfaceSectionHeaderParagraph>
+          </InterfaceSectionHeader>
+
+          <div className="p-6 grid grid-cols-1 gap-3 lg:grid-cols-2">
+            <div className="text-white text-sm">
+              <p className="lg:mt-4 mb-2">
+                Ensure you replace the baseURL with our API's URL and include
+                your{" "}
+                <Link
+                  to={routes.organizationSettings(organizationId)}
+                  className="text-primary-500 hover:underline"
+                  target="_blank"
+                >
+                  API key
+                </Link>{" "}
+                as the bearer token in the Authorization header.
+              </p>
+            </div>
+            <div className="w-full">
+              <CodePreviewWrapper
+                wrapLines
+                value={`curl "${apiUrl}/api/organizations/${organizationId}/memory_collections/${collectionId}/search?query=your_search_term_here" \\
+  -X GET \\
+  -H "Content-Type: application/json" \\
+  -H "Authorization: Bearer \${BUILDEL_API_KEY}"`}
+                language="shell"
+                height={122}
+              >
+                {(value) => <CopyCodeButton value={value} />}
+              </CodePreviewWrapper>
+            </div>
+          </div>
+        </InterfaceSectionWrapper>
+      </div>
 
       <ActionSidebar
         className={classNames("!bg-neutral-950 md:w-[550px]")}
