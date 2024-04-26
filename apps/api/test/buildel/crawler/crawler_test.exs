af2aaadb0ee42e39ac43df5707ce95f54e79f62d
Michał <mw@grifel.dev>
2024-04-26 11:21:10 +0200

    Add vcr

--- apps/api/test/buildel/crawler/crawler_test.exs
+++ apps/api/test/buildel/crawler/crawler_test.exs
@@ -1,5 +1,6 @@
 defmodule Buildel.Crawler.CrawlerTest do
   use ExUnit.Case, async: true
+  use ExVCR.Mock, adapter: ExVCR.Adapter.Hackney
 
   describe "crawl" do
     test "returns error when url is invalid" do
@@ -7,14 +8,18 @@ defmodule Buildel.Crawler.CrawlerTest do
     end
 
     test "returns error when request fails" do
-      assert Buildel.Crawler.crawl("http://nonexistentwebsite1234567890.com") ==
-               {:error, :request_failed}
+      use_cassette("crawler_http_fail") do
+        assert Buildel.Crawler.crawl("http://nonexistentwebsite1234567890.com") ==
+                 {:error, :request_failed}
+      end
     end
 
     test "returns ok  when request succeeds" do
-      url = "http://example.com"
-      assert {:ok, %{body: body, url: ^url}} = Buildel.Crawler.crawl(url)
-      assert String.contains?(body, "<html>")
+      use_cassette("crawler_http_example") do
+        url = "http://example.com"
+        assert {:ok, %{body: body, url: ^url}} = Buildel.Crawler.crawl(url)
+        assert String.contains?(body, "<html>")
+      end
     end
   end
 end
