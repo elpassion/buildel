8c6e25018899a6bd5f077aa8a5dc8f4045be0736
pawelsierant <paw9810@gmail.com>
2024-06-26 17:39:45 +0200

    update index migration for collection chunks

--- apps/api/priv/repo/migrations/20240626125918_create_vector_collection_chunks_index.exs
+++ apps/api/priv/repo/migrations/20240626125918_create_vector_collection_chunks_index.exs
@@ -1,8 +1,15 @@
 defmodule Buildel.Repo.Migrations.CreateVectorCollectionChunksIndex do
   use Ecto.Migration
 
-  def change do
+  def up do
     execute "ALTER EXTENSION vector UPDATE;"
+    execute "CREATE INDEX ON vector_collection_chunks (collection_name);"
+
     execute "CREATE INDEX ON vector_collection_chunks USING hnsw ((embedding_3072::halfvec(3072)) halfvec_l2_ops);"
+  end
+
+  def down do
+    execute "DROP INDEX IF EXISTS vector_collection_chunks_collection_name_idx;"
+    execute "DROP INDEX IF EXISTS vector_collection_chunks_embedding_3072_idx;"
   end
 end
