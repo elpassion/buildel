bd0624008095e05079bcd0360d9d74f5961c05ed
Michał <mw@grifel.dev>
2024-07-01 18:20:07 +0200

    Simplify migration

--- apps/api/priv/repo/migrations/20240701081447_update_pipeline_interface_config.exs
+++ apps/api/priv/repo/migrations/20240701081447_update_pipeline_interface_config.exs
@@ -23,26 +23,6 @@ defmodule Buildel.Repo.Migrations.UpdatePipelineInterfaceConfig do
     end)
   end
 
-  def down do
-    Buildel.Repo.transaction(fn ->
-      Buildel.Repo.all(Buildel.Pipelines.Pipeline)
-      |> Enum.map(fn pipeline ->
-        Pipeline.changeset(pipeline, %{
-          interface_config: update_interface_config_down(pipeline.interface_config)
-        })
-        |> Buildel.Repo.update!()
-      end)
-
-      Buildel.Repo.all(Buildel.Pipelines.Alias)
-      |> Enum.map(fn alias ->
-        Alias.changeset(alias, %{
-          interface_config: update_interface_config_down(alias.interface_config)
-        })
-        |> Buildel.Repo.update!()
-      end)
-    end)
-  end
-
   defp update_interface_config_up(interface_config) do
     case interface_config do
       nil ->
@@ -55,32 +35,17 @@ defmodule Buildel.Repo.Migrations.UpdatePipelineInterfaceConfig do
         text =
           if is_nil(Map.get(config, "input")),
             do: [],
-            else: [
-              %{
-                name: Map.get(config, "input"),
-                type: "text_input"
-              }
-            ]
+            else: [Map.get(config, "input")]
 
         file =
           if is_nil(Map.get(config, "file")),
             do: [],
-            else: [
-              %{
-                name: Map.get(config, "file"),
-                type: "file_input"
-              }
-            ]
+            else: [Map.get(config, "file")]
 
         outputs =
           if is_nil(Map.get(config, "output")),
             do: [],
-            else: [
-              %{
-                name: Map.get(config, "output"),
-                type: "text_output"
-              }
-            ]
+            else: [Map.get(config, "output")]
 
         %{
           webchat: %{
@@ -88,30 +53,6 @@ defmodule Buildel.Repo.Migrations.UpdatePipelineInterfaceConfig do
             outputs: outputs,
             public: Map.get(config, "public", false)
           }
-        }
-    end
-  end
-
-  defp update_interface_config_down(interface_config) do
-    case interface_config do
-      nil ->
-        nil
-
-      config when config == %{} ->
-        %{}
-
-      config ->
-        inputs = Map.get(config["webchat"], "inputs", [])
-        outputs = Map.get(config["webchat"], "outputs", [])
-
-        text_input = Enum.find(inputs, %{}, fn input -> input["type"] == "text_input" end)
-        file_input = Enum.find(inputs, %{}, fn input -> input["type"] == "file_input" end)
-
-        %{
-          "input" => Map.get(text_input, "name"),
-          "file" => Map.get(file_input, "name"),
-          "output" => Map.get(List.first(outputs, %{}), "name"),
-          "public" => Map.get(config["webchat"], "public", false)
         }
     end
   end
