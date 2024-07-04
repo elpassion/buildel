3488983ccdf9eddd4b76d235d0b85a57a7c8e294
Monika Zyznowska <monika.zyznowska@elpassion.pl>
2024-07-04 15:02:40 +0200

    Add summary cost to bulk

--- apps/web-remix/app/components/pages/pipelines/interface/bulk/page.tsx
+++ apps/web-remix/app/components/pages/pipelines/interface/bulk/page.tsx
@@ -31,6 +31,9 @@ export function BulkPage() {
   const validator = useMemo(() => withZod(schema), []);
   const [selectedInputs, setSelectedInputs] = useState<string[]>([]);
   const [selectedOutputs, setSelectedOutputs] = useState<string[]>([]);
+  const [summaryRunCost, setSummaryRunCost] = useState<
+    { id: string; cost: number }[]
+  >([]);
 
   const inputs = pipeline.config.blocks.filter(
     (block) => block.type === "text_input",
@@ -129,7 +132,17 @@ export function BulkPage() {
       {
         method: "POST",
       },
-    );
+    )
+      .then((res) => res.json())
+      .then((stopData) => {
+        const newSummaryRunCost = summaryRunCost.map((runCost) =>
+          runCost.id === id
+            ? { id, cost: Number(stopData.data.total_cost) }
+            : runCost,
+        );
+
+        setSummaryRunCost(newSummaryRunCost);
+      });
   };
 
   const handleOnSubmit = async (e: any) => {
@@ -323,129 +336,134 @@ export function BulkPage() {
             </div>
           </ValidatedForm>
           {(selectedInputs.length > 0 || selectedOutputs.length > 0) && (
-            <table className="w-full table-auto">
-              <thead className="text-left text-white text-xs bg-neutral-800">
-                <tr className="rounded-xl overflow-hidden">
-                  {selectedInputs?.map((input: string) => (
-                    <th
-                      key={input}
-                      className="py-3 px-5 first:rounded-tl-lg first:rounded-bl-lg last:rounded-tr-lg last:rounded-br-lg"
-                    >
-                      {input}
-                    </th>
-                  ))}
-                  {selectedOutputs?.map((output: string) => (
-                    <th
-                      key={output}
-                      className="py-3 px-5 first:rounded-tl-lg first:rounded-bl-lg last:rounded-tr-lg last:rounded-br-lg"
-                    >
-                      {output}
-                    </th>
-                  ))}
-                  {tests.length > 1 && (
-                    <th className="py-3 px-5 first:rounded-tl-lg first:rounded-bl-lg last:rounded-tr-lg last:rounded-br-lg"></th>
-                  )}
-                  {tests.find((test) => test?.run) && (
-                    <th className="py-3 px-5 first:rounded-tl-lg first:rounded-bl-lg last:rounded-tr-lg last:rounded-br-lg"></th>
-                  )}
-                </tr>
-              </thead>
-              <tbody>
-                {tests.map((test) => {
-                  return (
-                    <tr
-                      key={test.id}
-                      className={classNames(
-                        "[&:not(:first-child)]:border-t border-neutral-800 rounded-sm overflow-hidden",
-                        {
-                          "bg-primary-500": test.status === "running",
-                          "bg-neutral-8800": test.status === "done",
-                        },
-                      )}
-                      aria-label="pipeline run"
-                    >
-                      {selectedInputs.map((input) => (
-                        <td
-                          key={input}
-                          className="py-3 px-5 text-neutral-100 text-sm"
-                        >
-                          <TextareaInput
-                            id={input}
+            <>
+              <p className="text-white">
+                <span className="text-neutral-100">Summary cost ($): </span>
+                {summaryRunCost.reduce((acc, runCost) => acc + runCost.cost, 0)}
+              </p>
+              <table className="w-full table-auto">
+                <thead className="text-left text-white text-xs bg-neutral-800">
+                  <tr className="rounded-xl overflow-hidden">
+                    {selectedInputs?.map((input: string) => (
+                      <th
+                        key={input}
+                        className="py-3 px-5 first:rounded-tl-lg first:rounded-bl-lg last:rounded-tr-lg last:rounded-br-lg"
+                      >
+                        {input}
+                      </th>
+                    ))}
+                    {selectedOutputs?.map((output: string) => (
+                      <th
+                        key={output}
+                        className="py-3 px-5 first:rounded-tl-lg first:rounded-bl-lg last:rounded-tr-lg last:rounded-br-lg"
+                      >
+                        {output}
+                      </th>
+                    ))}
+                    {tests.length > 1 && (
+                      <th className="py-3 px-5 first:rounded-tl-lg first:rounded-bl-lg last:rounded-tr-lg last:rounded-br-lg"></th>
+                    )}
+                    {tests.find((test) => test?.run) && (
+                      <th className="py-3 px-5 first:rounded-tl-lg first:rounded-bl-lg last:rounded-tr-lg last:rounded-br-lg"></th>
+                    )}
+                  </tr>
+                </thead>
+                <tbody>
+                  {tests.map((test) => {
+                    return (
+                      <tr
+                        key={test.id}
+                        className={classNames(
+                          "[&:not(:first-child)]:border-t border-neutral-800 rounded-sm overflow-hidden",
+                          {
+                            "bg-primary-500": test.status === "running",
+                            "bg-neutral-8800": test.status === "done",
+                          },
+                        )}
+                        aria-label="pipeline run"
+                      >
+                        {selectedInputs.map((input) => (
+                          <td
                             key={input}
-                            value={test.inputs[input]}
-                            className="inline-block h-full"
-                            areaClassName="min-h-full !resize-y"
-                            onChange={(e) => {
-                              e.preventDefault();
-                              const value = e.target.value;
-
-                              setTests((tests) =>
-                                tests.map((t) =>
-                                  test.id === t.id
-                                    ? {
-                                        ...test,
-                                        inputs: {
-                                          ...test.inputs,
-                                          [input]: value,
-                                        },
-                                      }
-                                    : t,
-                                ),
-                              );
-                            }}
-                          />
-                        </td>
-                      ))}
-                      {selectedOutputs.map((output) => (
-                        <td
-                          key={output}
-                          className="py-3 px-5 text-neutral-100 text-sm w-[40%]"
-                        >
-                          <ChatMarkdown>{test.outputs[output]}</ChatMarkdown>
-                        </td>
-                      ))}
-                      {tests.length > 1 && (
-                        <td className="w-7 py-3 text-neutral-100 text-sm">
-                          <IconButton
-                            size="xs"
-                            variant="basic"
-                            aria-label={`Remove item`}
-                            className="!bg-neutral-700 !text-white !text-sm hover:!text-red-500 mt-4"
-                            title={`Remove item`}
-                            icon={<Icon iconName="trash" />}
-                            onClick={() =>
-                              setTests((tests) =>
-                                tests.filter(({ id }) => id !== test.id),
-                              )
-                            }
-                          />
-                        </td>
-                      )}
-                      {test.run && (
-                        <td className="w-7 py-3">
-                          <Link
-                            id={`run-link-${test.run}`}
-                            to={routes.pipelineRun(
-                              organizationId,
-                              pipelineId,
-                              test.run,
-                            )}
+                            className="py-3 px-5 text-neutral-100 text-sm"
                           >
-                            <IconButton
-                              className="!bg-neutral-700 !text-white !text-sm hover:!text-red-500 mt-4"
-                              variant="basic"
-                              aria-label="Go to run overview"
-                              icon={<Icon iconName="external-link" />}
-                              size="xs"
+                            <TextareaInput
+                              id={input}
+                              key={input}
+                              value={test.inputs[input]}
+                              areaClassName="min-h-full !resize-y"
+                              onChange={(e) => {
+                                e.preventDefault();
+                                const value = e.target.value;
+
+                                setTests((tests) =>
+                                  tests.map((t) =>
+                                    test.id === t.id
+                                      ? {
+                                          ...test,
+                                          inputs: {
+                                            ...test.inputs,
+                                            [input]: value,
+                                          },
+                                        }
+                                      : t,
+                                  ),
+                                );
+                              }}
                             />
-                          </Link>
-                        </td>
-                      )}
-                    </tr>
-                  );
-                })}
-              </tbody>
-            </table>
+                          </td>
+                        ))}
+                        {selectedOutputs.map((output) => (
+                          <td
+                            key={output}
+                            className="py-3 px-5 text-neutral-100 text-sm w-[40%]"
+                          >
+                            <ChatMarkdown>{test.outputs[output]}</ChatMarkdown>
+                          </td>
+                        ))}
+                        {tests.length > 1 && (
+                          <td className="w-7 py-3 text-neutral-100 text-sm">
+                            <IconButton
+                              size="xs"
+                              variant="basic"
+                              aria-label={`Remove item`}
+                              className="!bg-neutral-700 !text-white !text-sm hover:!text-red-500 mt-4"
+                              title={`Remove item`}
+                              icon={<Icon iconName="trash" />}
+                              onClick={() =>
+                                setTests((tests) =>
+                                  tests.filter(({ id }) => id !== test.id),
+                                )
+                              }
+                            />
+                          </td>
+                        )}
+                        {test.run && (
+                          <td className="w-7 py-3">
+                            <Link
+                              id={`run-link-${test.run}`}
+                              to={routes.pipelineRun(
+                                organizationId,
+                                pipelineId,
+                                test.run,
+                              )}
+                            >
+                              <IconButton
+                                className="!bg-neutral-700 !text-white !text-sm hover:!text-red-500 mt-4"
+                                variant="basic"
+                                aria-label="Go to run overview"
+                                icon={<Icon iconName="external-link" />}
+                                size="xs"
+                              />
+                            </Link>
+                          </td>
+                        )}
+                      </tr>
+                    );
+                  })}
+                </tbody>
+              </table>
+            </>
           )}
           {(selectedInputs.length > 0 || selectedOutputs.length > 0) && (
             <IconButton
