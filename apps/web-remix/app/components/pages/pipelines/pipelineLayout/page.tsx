f044cd2aaded060c711ec12ecdb42219c64e7601
kielbasa-elp <dawid.kielbasa@elpassion.pl>
2024-07-30 14:45:20 +0200

    use link instead of button

--- apps/web-remix/app/components/pages/pipelines/pipelineLayout/page.tsx
+++ apps/web-remix/app/components/pages/pipelines/pipelineLayout/page.tsx
@@ -1,12 +1,8 @@
 import React from 'react';
-import {
-  Outlet,
-  useLoaderData,
-  useNavigate,
-  useSearchParams,
-} from '@remix-run/react';
+import { Outlet, useLoaderData, useSearchParams } from '@remix-run/react';
 import { ChevronLeft } from 'lucide-react';
 
+import { BasicLink } from '~/components/link/BasicLink';
 import { PipelineName } from '~/components/pages/pipelines/pipelineLayout/PipelineName';
 import { PipelineLayoutHeader } from '~/components/pages/pipelines/PipelineLayoutHeader';
 import { FilledTabLink } from '~/components/tabs/FilledTabLink';
@@ -20,29 +16,21 @@ import { AliasSelect, CreateAliasForm, RestoreWorkflow } from './Aliases';
 import type { loader } from './loader.server';
 
 export function PipelineLayout() {
-  const navigate = useNavigate();
   const { toasts, pipeline, organizationId, aliases, aliasId } =
     useLoaderData<typeof loader>();
   const [searchParams] = useSearchParams();
 
   useServerToasts(toasts);
 
-  const backToWorkflows = () => {
-    navigate(routes.pipelines(organizationId));
-  };
-
   return (
     <>
       <TabGroup>
         <PipelineLayoutHeader className="lg:grid lg:grid-cols-[360px_1fr_360px]">
           <div className="flex gap-2 items-center">
-            <Button
-              variant="secondary"
-              size="xs"
-              className="w-8 h-8"
-              onClick={backToWorkflows}
-            >
-              <ChevronLeft className="w-5 h-5" />
+            <Button asChild variant="secondary" size="xs" className="w-8 h-8">
+              <BasicLink to={routes.pipelines(organizationId)}>
+                <ChevronLeft className="w-5 h-5" />
+              </BasicLink>
             </Button>
 
             <FilledTabsWrapper size="xs">
