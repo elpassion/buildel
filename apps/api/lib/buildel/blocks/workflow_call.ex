b6188ff7339c03249e8aa377b0ab3c9be6880deb
pawelsierant <paw9810@gmail.com>
2024-07-31 15:24:33 +0200

    Add processing external inputs

--- apps/api/lib/buildel/blocks/workflow_call.ex
+++ apps/api/lib/buildel/blocks/workflow_call.ex
@@ -11,7 +11,7 @@ defmodule Buildel.Blocks.WorkflowCall do
       type: "workflow_call",
       description: "A block that allows you to call a workflow from within another workflow.",
       groups: ["tools"],
-      inputs: [],
+      inputs: [Block.text_input("stop_run")],
       outputs: [],
       ios: [],
       dynamic_ios: "/api/organizations/{{organization_id}}/pipelines/{{opts.workflow}}/ios",
@@ -58,135 +58,47 @@ defmodule Buildel.Blocks.WorkflowCall do
     {:ok, state |> Map.put(:workflow_id, workflow_id)}
   end
 
+  def handle_external_input(topic, {_, message_type, payload, metadata}, state) do
+    context = BlockPubSub.io_from_topic(topic)
+
+    output(state, context.block <> ":" <> context.io, {message_type, payload}, %{
+      metadata: metadata,
+      stream_stop: :none
+    })
+  end
+
+  def handle_stream_stop({topic, :stop_stream, _, _metadata}, state) do
+    context = BlockPubSub.io_from_topic(topic)
+
+    state = send_stream_stop(state, context.block <> ":" <> context.io)
+    {:noreply, state}
+  end
+
   @impl true
-  def handle_input(input_name, {_name, type, data, metadata} = payload, state) do
-    IO.inspect(input_name)
-    IO.inspect(payload, label: "payload")
+  def handle_input("stop_run", {_name, :text, _data, _metadata}, state) do
+    Pipelines.Runner.stop_run(state[:run])
+    state
+  end
+
+  @impl true
+  def handle_input(input_name, {_name, type, data, metadata}, state) do
     [block_name, input_name] = String.split(input_name, ":")
 
     %{global: organization_id} = block_context().context_from_context_id(state[:context_id])
     run = create_and_start_run(organization_id, state.workflow_id, %{})
-    listen_to_outputs(run) |> IO.inspect(label: "listen_to_outputs")
-
-    opts = Map.merge(%{stream_stop: :send, metadata: metadata}, %{})
+    state = state |> Map.put(:run, run)
+    listen_to_outputs(run)
 
     Buildel.BlockPubSub.broadcast_to_io(
-      Pipelines.Worker.context_id(run),
+      Pipelines.Worker.context_id(state[:run]),
       block_name,
       input_name,
       {type, data},
-      opts.metadata
+      metadata
       |> Map.put_new_lazy(:message_id, fn -> state[:message_id] || UUID.uuid4() end)
     )
 
-    pipeline = Pipelines.get_pipeline!(state.workflow_id)
-
-    %{data: %{outputs: outputs}} =
-      BuildelWeb.OrganizationPipelineJSON.ios(%{pipeline: pipeline})
-
-    public_outputs =
-      outputs
-      |> Enum.filter(fn output -> output.public end)
-      |> Enum.map(fn output ->
-        [block_name, output_name] = String.split(output.name, ":")
-
-        %{
-          block_name: block_name,
-          output_name: output_name,
-          topic:
-            BlockPubSub.io_topic(
-              Pipelines.Worker.context_id(run),
-              block_name,
-              output_name
-            ),
-          data: nil,
-          finished: false
-        }
-      end)
-
-    IO.inspect(public_outputs, label: "public_outputs")
-
-    results =
-      Enum.reduce_while(Stream.repeatedly(fn -> nil end), public_outputs, fn _, outputs ->
-        outputs = receive_output(outputs)
-
-        outputs =
-          outputs
-          |> Enum.map(fn output ->
-            case output.data do
-              nil ->
-                output
-
-              data ->
-                case output.finished do
-                  false ->
-                    IO.inspect(data, label: "data")
-                    output(state, output.block_name <> ":" <> output.output_name, {:text, data})
-                    Map.put(output, :finished, true)
-
-                  true ->
-                    output
-                end
-            end
-          end)
-
-        if Enum.any?(outputs, &(&1.data == nil)) do
-          {:cont, outputs}
-        else
-          {:halt, outputs}
-        end
-      end)
-
     state
-    # output(state, "text_output_2:output", {:text, data})
-  end
-
-  @impl true
-  def handle_input("input", {_name, :text, text, _metadata} = payload, state) do
-    IO.inspect(payload, label: "payload")
-    IO.inspect(state, label: "state")
-    # %{global: organization_id} = block_context().context_from_context_id(state[:context_id])
-    # run = create_and_start_run(organization_id, state.workflow_id, %{})
-    # listen_to_outputs(run) |> IO.inspect(label: "listen_to_outputs")
-
-    # opts = Map.merge(%{stream_stop: :send, metadata: %{}}, %{})
-
-    # Buildel.BlockPubSub.broadcast_to_io(
-    #   Pipelines.Worker.context_id(run),
-    #   "text_input_1",
-    #   "input",
-    #   {:text, text},
-    #   opts.metadata
-    #   |> Map.put_new_lazy(:message_id, fn -> state[:message_id] || UUID.uuid4() end)
-    # )
-
-    # outputs = [
-    #   %{
-    #     block_name: "text_output_1",
-    #     output_name: "output",
-    #     topic:
-    #       BlockPubSub.io_topic(
-    #         Pipelines.Worker.context_id(run),
-    #         "text_output_1",
-    #         "output"
-    #       ),
-    #     data: nil
-    #   }
-    # ]
-
-    # results =
-    #   Enum.reduce_while(Stream.repeatedly(fn -> nil end), outputs, fn _, outputs ->
-    #     outputs = receive_output(outputs)
-
-    #     if Enum.any?(outputs, &(&1.data == nil)) do
-    #       {:cont, outputs}
-    #     else
-    #       {:halt, outputs}
-    #     end
-    #   end)
-    #   |> IO.inspect(label: "outputs")
-
-    output(state, "output", {:text, text})
   end
 
   defp create_and_start_run(organization_id, pipeline_id, metadata) do
@@ -214,36 +126,11 @@ defmodule Buildel.Blocks.WorkflowCall do
     |> Pipelines.blocks_for_run()
     |> Enum.map(fn block ->
       public_outputs = block.type.options.outputs |> Enum.filter(fn output -> output.public end)
-      Buildel.BlockPubSub.subscribe_to_block(context_id, block.name)
+      # Buildel.BlockPubSub.subscribe_to_block(context_id, block.name)
 
       for output <- public_outputs do
         Buildel.BlockPubSub.subscribe_to_io(context_id, block.name, output.name)
       end
     end)
-  end
-
-  defp receive_output([]), do: []
-
-  defp receive_output(outputs) do
-    topics = outputs |> Enum.map(& &1[:topic])
-
-    receive do
-      {topic, type, data, _metadata} when type != :start_stream and type != :stop_stream ->
-        if topic in topics do
-          outputs
-          |> update_in(
-            [
-              Access.at(Enum.find_index(outputs, fn output -> output[:topic] == topic end)),
-              :data
-            ],
-            fn _ -> data end
-          )
-        else
-          outputs
-        end
-
-      _other ->
-        outputs
-    end
   end
 end
