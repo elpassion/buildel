364cbe01e443747c9adab74137ec54bca4d0dd40
kielbasa-elp <dawid.kielbasa@elpassion.pl>
2024-08-07 21:49:19 +0200

    replace rbg by hsl

--- apps/web-remix/app/components/pages/knowledgeBase/collectionGraph/collectionGraph.utils.ts
+++ apps/web-remix/app/components/pages/knowledgeBase/collectionGraph/collectionGraph.utils.ts
@@ -9,7 +9,7 @@ export function toEmbeddingNodes(nodes: IMemoryNode[]): IEmbeddingNode[] {
       ...item,
       base_color: getColorForUid(item.memory_id.toString()),
     },
-    position: { x: item.point[0] * 25, y: item.point[1] * 25 },
+    position: { x: item.point[0] * 35, y: item.point[1] * 35 },
     id: item.id.toString(),
     type: 'embedding',
   }));
@@ -20,17 +20,19 @@ const colors: Record<string, string> = {};
 function getColorForUid(uid: string): string {
   if (colors[uid]) return colors[uid];
 
-  const rgb = generateRGBColor(hashString(uid));
+  const hsl = generateHSLColor(hashString(uid));
 
-  colors[uid] = rgb;
+  colors[uid] = hsl;
 
-  return rgb;
+  return hsl;
 }
 
-function generateRGBColor(hash: number): string {
-  const r = Math.abs(hash * 0.7) % 240;
-  const g = Math.abs(hash * 5.3 * hash) % 240;
-  const b = Math.abs(hash * 8.3 * hash) % 240;
+const GOLDEN_RATIO_CONJUGATE = 0.618033988749895;
 
-  return `rgb(${r}, ${g}, ${b})`;
+function generateHSLColor(hash: number): string {
+  const hue = (Math.abs(hash * GOLDEN_RATIO_CONJUGATE) % 1.0) * 360;
+  const saturation = 50 + (Math.abs(hash * 0.3) % 50);
+  const lightness = 50 + (Math.abs(hash * 0.2) % 30);
+
+  return `hsl(${hue}, ${saturation}%, ${lightness}%)`;
 }
