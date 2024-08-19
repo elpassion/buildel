ad58e194bea8f159f50444882105e6ee815aa3d9
kielbasa-elp <dawid.kielbasa@elpassion.pl>
2024-08-19 13:30:27 +0200

    added terms/privacy policy agreement to register page

--- apps/web-remix/app/components/pages/auth/register/page.tsx
+++ apps/web-remix/app/components/pages/auth/register/page.tsx
@@ -27,66 +27,88 @@ export function RegisterPage() {
   const redirectTo = searchParams.get('redirectTo');
 
   return (
-    <div className="my-auto flex flex-col w-full justify-center items-center">
-      <h1 className="text-center text-3xl font-bold text-foreground">
-        Register for an account
-      </h1>
-      <p className="text-center text-muted-foreground">
-        Already registered?{' '}
-        <Link
-          to={{
-            pathname: '/login',
-            search: searchParams.toString(),
-          }}
-          className="text-foreground"
+    <>
+      <div className="my-auto flex flex-col w-full justify-center items-center">
+        <h1 className="text-center text-3xl font-bold text-foreground">
+          Register for an account
+        </h1>
+        <p className="text-center text-muted-foreground">
+          Already registered?{' '}
+          <Link
+            to={{
+              pathname: '/login',
+              search: searchParams.toString(),
+            }}
+            className="text-foreground"
+          >
+            Sign in
+          </Link>{' '}
+          to your account now.
+        </p>
+        <ValidatedForm
+          validator={validator}
+          method="post"
+          noValidate
+          className="w-full max-w-md mt-10"
         >
-          Sign in
-        </Link>{' '}
-        to your account now.
+          <HiddenField name="redirectTo" value={redirectTo ?? undefined} />
+
+          <div className="form-control w-full mb-4">
+            <Field name="user.email">
+              <FieldLabel>Email address</FieldLabel>
+              <TextInputField aria-label="email" type="email" autoFocus />
+              <FieldMessage />
+            </Field>
+          </div>
+          <div className="max-w-s form-control w-full mb-2">
+            <Field name="user.password">
+              <FieldLabel>Password</FieldLabel>
+              <PasswordInputField aria-label="password" />
+              <FieldMessage />
+            </Field>
+          </div>
+          <div className="mb-4">
+            <Field name="global">
+              <FieldMessage />
+            </Field>
+          </div>
+
+          <SubmitButton isFluid>Register</SubmitButton>
+        </ValidatedForm>
+
+        {googleLoginEnabled && (
+          <>
+            <span className="my-3 text-muted-foreground text-sm">Or</span>
+            <SocialSignInForm action="/auth/google" className="max-w-md mb-4">
+              <GoogleButton content="Sign up with Google" />
+            </SocialSignInForm>
+            <SocialSignInForm action="/auth/github" className="max-w-md">
+              <GithubButton content="Sign up with Github" />
+            </SocialSignInForm>
+          </>
+        )}
+      </div>
+
+      <p className="text-xs text-muted-foreground text-center mt-6">
+        By signing up you agree to our{' '}
+        <a
+          href="https://buildel.ai/terms-and-conditions"
+          target="_blank"
+          className="font-semibold hover:underline"
+        >
+          terms of service
+        </a>{' '}
+        and{' '}
+        <a
+          href="https://buildel.ai/privacy-policy"
+          target="_blank"
+          className="font-semibold hover:underline"
+        >
+          privacy policy
+        </a>
+        .
       </p>
-      <ValidatedForm
-        validator={validator}
-        method="post"
-        noValidate
-        className="w-full max-w-md mt-10"
-      >
-        <HiddenField name="redirectTo" value={redirectTo ?? undefined} />
-
-        <div className="form-control w-full mb-4">
-          <Field name="user.email">
-            <FieldLabel>Email address</FieldLabel>
-            <TextInputField aria-label="email" type="email" autoFocus />
-            <FieldMessage />
-          </Field>
-        </div>
-        <div className="max-w-s form-control w-full mb-2">
-          <Field name="user.password">
-            <FieldLabel>Password</FieldLabel>
-            <PasswordInputField aria-label="password" />
-            <FieldMessage />
-          </Field>
-        </div>
-        <div className="mb-4">
-          <Field name="global">
-            <FieldMessage />
-          </Field>
-        </div>
-
-        <SubmitButton isFluid>Register</SubmitButton>
-      </ValidatedForm>
-
-      {googleLoginEnabled && (
-        <>
-          <span className="my-3 text-muted-foreground text-sm">Or</span>
-          <SocialSignInForm action="/auth/google" className="max-w-md mb-4">
-            <GoogleButton content="Sign up with Google" />
-          </SocialSignInForm>
-          <SocialSignInForm action="/auth/github" className="max-w-md">
-            <GithubButton content="Sign up with Github" />
-          </SocialSignInForm>
-        </>
-      )}
-    </div>
+    </>
   );
 }
 
