3ade812464b3a629628ec25cb4ec0e539274c5b7
kielbasa-elp <dawid.kielbasa@elpassion.pl>
2024-08-26 15:37:20 +0200

    set utf-8 character encoding

--- apps/web-remix/app/components/layout/Document.tsx
+++ apps/web-remix/app/components/layout/Document.tsx
@@ -13,6 +13,8 @@ export function Document({
       <head>
         <meta charSet="utf-8" />
         <meta name="viewport" content="width=device-width,initial-scale=1" />
+        <meta httpEquiv=" Content-Type" content="text/html;charset=utf-8" />
+
         <Meta />
         <Links />
         <script
