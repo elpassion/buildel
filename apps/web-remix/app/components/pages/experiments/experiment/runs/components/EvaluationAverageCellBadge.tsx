faa6fa70d7d7b6557816514273415da2ace7b20e
kielbasa-elp <dawid.kielbasa@elpassion.pl>
2024-08-27 19:33:53 +0200

    fixed table avg result

--- apps/web-remix/app/components/pages/experiments/experiment/runs/components/EvaluationAverageCellBadge.tsx
+++ apps/web-remix/app/components/pages/experiments/experiment/runs/components/EvaluationAverageCellBadge.tsx
@@ -8,9 +8,7 @@ export const EvaluationAverageCellBadge = <TData, TValue>(
 ) => {
   const value = info.getValue();
 
-  if (!value) return '';
-
-  if (typeof value === 'number')
+  if (typeof value === 'number') {
     return (
       <Badge
         variant={value < 25 ? 'error' : value >= 75 ? 'success' : 'warning'}
@@ -18,6 +16,7 @@ export const EvaluationAverageCellBadge = <TData, TValue>(
         {value}%
       </Badge>
     );
+  }
 
   return info.getValue();
 };
