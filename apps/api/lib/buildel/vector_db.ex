c360d0b0ed63c34279e88b00835de11319367bf8
Michał <mw@grifel.dev>
2024-09-08 10:58:01 +0200

    Remove unneeded filter for embedding columns

--- apps/api/lib/buildel/vector_db.ex
+++ apps/api/lib/buildel/vector_db.ex
@@ -325,7 +325,6 @@ defmodule Buildel.VectorDB.EctoAdapter do
     if chunk do
       embedding_column =
         Map.keys(chunk)
-        |> Enum.filter(fn key -> !String.starts_with?(to_string(key), "embedding_reduced") end)
         |> Enum.filter(fn key -> String.starts_with?(to_string(key), "embedding_") end)
         |> Enum.filter(fn key -> chunk |> Map.get(key) |> is_struct(Pgvector) end)
         |> List.first()
