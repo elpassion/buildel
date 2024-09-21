522130dcc95bd69a593d954440139c3412411063
Michal Warda <t1du53k@gmail.com>
2024-09-21 16:22:09 +0200

    Turn off logs

--- apps/api/lib/buildel/application.ex
+++ apps/api/lib/buildel/application.ex
@@ -31,8 +31,8 @@ defmodule Buildel.Application do
           {Finch, name: Buildel.Finch},
           # Start the Logs system
           Buildel.Logs,
-          Buildel.Logs.DBPipelineLogger,
-          Buildel.LogsAggregator,
+          # Buildel.Logs.DBPipelineLogger,
+          # Buildel.LogsAggregator,
           # Start the Endpoint (http/https)
           BuildelWeb.Endpoint,
           # Start a worker by calling: Buildel.Worker.start_link(arg)
