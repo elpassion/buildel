cf74adde574ff00126c4bc32ca85d3b43ee7db13
breeg554 <dvdk98@gmail.com>
2024-09-23 16:53:19 +0200

    tmp: fix lint

--- apps/web-remix/app/routes/voice-chat.$organizationId.pipelines.$pipelineId.tsx
+++ apps/web-remix/app/routes/voice-chat.$organizationId.pipelines.$pipelineId.tsx
@@ -1,10 +1,13 @@
-import React from 'react';
+import React, { useState } from 'react';
 import type { LoaderFunctionArgs } from '@remix-run/node';
-import { useLoaderData } from '@remix-run/react';
+import { Mic } from 'lucide-react';
 import invariant from 'tiny-invariant';
 
+import { MediaRecorderState } from '~/components/audioRecorder/AudioRecorder';
+import { useAudioRecorder } from '~/components/audioRecorder/useAudioRecorder';
 import { publicInterfaceLoader } from '~/components/pages/pipelines/interface/interface.loader.server';
 import { loaderBuilder } from '~/utils.server';
+import { cn } from '~/utils/cn';
 
 export async function loader(args: LoaderFunctionArgs) {
   return loaderBuilder(async ({ params, ...rest }, helpers) => {
@@ -16,19 +19,48 @@ export async function loader(args: LoaderFunctionArgs) {
 }
 
 export default function WebsiteChat() {
-  const { pipelineId, organizationId, pipeline, alias, chatSize } =
-    useLoaderData<typeof loader>();
+  const [status, setStatus] = useState<MediaRecorderState>('inactive');
+
+  const onStart = () => {
+    setStatus('recording');
+    console.log('START');
+  };
+
+  const onStop = () => {
+    setStatus('inactive');
+    console.log('STOP');
+  };
+
+  const { stop, start } = useAudioRecorder({
+    onStop: onStop,
+    onStart: onStart,
+    onChunk: (chunk) => console.log('CHUNK', chunk),
+  });
+
+  const startStop = async () => {
+    if (status === 'recording') {
+      await stop();
+    } else {
+      await start();
+    }
+  };
 
   return (
     <div className="flex justify-center items-center h-[100dvh] w-full bg-secondary">
       VOICE CHAT
-      {/*<Webchat*/}
-      {/*  organizationId={organizationId}*/}
-      {/*  pipelineId={pipelineId}*/}
-      {/*  pipeline={pipeline}*/}
-      {/*  alias={alias}*/}
-      {/*  size={chatSize}*/}
-      {/*/>*/}
+      <button
+        type="button"
+        className={cn(
+          'w-6 h-6 flex items-center justify-center bg-primary rounded-md ',
+          {
+            'text-primary-foreground': status !== 'recording',
+            'text-red-500': status === 'recording',
+          },
+        )}
+        onClick={startStop}
+      >
+        <Mic />
+      </button>
     </div>
   );
 }
