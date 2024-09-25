bf8bef95f6795666b78efb8e5f8d87e61ba2d6dd
pawelsierant <paw9810@gmail.com>
2024-09-25 13:16:44 +0200

    Add public output to text_input

--- apps/api/test/buildel/blocks/text_input_test.exs
+++ apps/api/test/buildel/blocks/text_input_test.exs
@@ -9,7 +9,7 @@ defmodule Buildel.Blocks.TextInputTest do
                description:
                  "This module is crafted for the seamless intake and transmission of textual data.",
                inputs: [Block.text_input("input", true), Block.text_input("forward")],
-               outputs: [Block.text_output("output")],
+               outputs: [Block.text_output("output"), Block.text_output("forward", true)],
                schema: TextInput.schema(),
                groups: ["text", "inputs / outputs"],
                ios: [],
@@ -37,11 +37,17 @@ defmodule Buildel.Blocks.TextInputTest do
         })
 
       {:ok, topic} = test_run |> BlocksTestRunner.Run.subscribe_to_output("test", "output")
+
+      {:ok, public_topic} =
+        test_run |> BlocksTestRunner.Run.subscribe_to_output("test", "forward")
+
       text = "text"
       test_run |> BlocksTestRunner.Run.input("test", "input", {:text, text})
       assert_receive {^topic, :start_stream, nil, _}
       assert_receive {^topic, :text, ^text, _}
       assert_receive {^topic, :stop_stream, nil, _}
+
+      assert_receive {^public_topic, :text, ^text, _}
     end
   end
 end
