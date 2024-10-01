d3b2444da1e5f5832d35675bbde9f7269fbebf90
pawelsierant <paw9810@gmail.com>
2024-10-01 09:30:55 +0200

    Remove new_text_to_speech

--- apps/api/lib/buildel/blocks.ex
+++ apps/api/lib/buildel/blocks.ex
@@ -41,7 +41,6 @@ defmodule Buildel.Blocks do
     Image,
     ImageOutput,
     Knowledge,
-    NewTextToSpeech,
     NewTextToSpeechElevenlabs,
     NewCollectAllText,
     CollectAllAudio
@@ -84,7 +83,6 @@ defmodule Buildel.Blocks do
     "image" => Image,
     "image_output" => ImageOutput,
     "knowledge" => Knowledge,
-    "new_text_to_speech" => NewTextToSpeech,
     "new_text_to_speech_elevenlabs" => NewTextToSpeechElevenlabs,
     "new_collect_all_text" => NewCollectAllText,
     "collect_all_audio" => CollectAllAudio
@@ -127,7 +125,6 @@ defmodule Buildel.Blocks do
     Image => "image",
     ImageOutput => "image_output",
     Knowledge => "knowledge",
-    NewTextToSpeech => "new_text_to_speech",
     NewTextToSpeechElevenlabs => "new_text_to_speech_elevenlabs",
     NewCollectAllText => "new_collect_all_text",
     CollectAllAudio => "collect_all_audio"
