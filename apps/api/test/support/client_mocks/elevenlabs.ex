b37d1116bc9c60daa1f1216287e36948c855fbe7
pawelsierant <paw9810@gmail.com>
2024-10-02 09:12:07 +0200

    Fix tts test

--- apps/api/test/support/client_mocks/elevenlabs.ex
+++ apps/api/test/support/client_mocks/elevenlabs.ex
@@ -2,43 +2,26 @@ defmodule Buildel.ClientMocks.Elevenlabs do
   alias Buildel.Clients.ElevenlabsBehaviour
   @behaviour ElevenlabsBehaviour
 
-  def init(state) do
-    {:ok, state}
+  @impl ElevenlabsBehaviour
+  def speak!(_api_key, _state) do
+    {:ok, self()}
   end
 
   @impl ElevenlabsBehaviour
-  def speak!(_api_key, state) do
-    GenServer.start_link(__MODULE__, state, []) |> IO.inspect(label: "speak!")
-  end
-
-  @impl ElevenlabsBehaviour
-  def generate_speech(pid, _text) do
-    IO.inspect("GENERATE_SPEECH")
-    IO.inspect(pid, label: "pid in generate_speech")
-
-    GenServer.cast(
-      pid,
+  def generate_speech(_pid, _text) do
+    send(
+      self(),
       {:audio_chunk, File.read!("test/support/fixtures/real.mp3")}
     )
 
     :ok
   end
 
-  def disconnect(pid) do
-    GenServer.stop(pid)
+  def disconnect(_pid) do
+    :ok
   end
 
-  def handle_cast({:audio_chunk, chunk}, state) do
-    IO.inspect("CAST")
-
-    IO.inspect(state.stream_to, label: "stream_to in handle_cast")
-
-    send(
-      state.stream_to,
-      {:audio_chunk, chunk}
-    )
-    |> IO.inspect()
-
-    {:noreply, state}
+  def flush(_pid) do
+    :ok
   end
 end
