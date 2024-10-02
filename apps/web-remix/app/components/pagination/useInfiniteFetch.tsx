18a9842930055649b9ecee611e035ec0e4d3ae28
breeg554 <dvdk98@gmail.com>
2024-10-02 16:52:35 +0200

    wip: get rid of usePagination

--- apps/web-remix/app/components/pagination/useInfiniteFetch.tsx
+++ apps/web-remix/app/components/pagination/useInfiniteFetch.tsx
@@ -1,11 +1,10 @@
-import { useEffect, useMemo, useState } from 'react';
+import { useEffect, useMemo, useReducer } from 'react';
 import type { SerializeFrom } from '@remix-run/node';
 import { useFetcher } from '@remix-run/react';
 import type { FetcherWithComponents } from '@remix-run/react';
 
 import { buildUrlWithParams } from '~/utils/url';
 
-import { usePagination } from './usePagination';
 import type { Pagination } from './usePagination';
 
 interface UseInfiniteFetchProps<T, R> {
@@ -19,62 +18,137 @@ interface UseInfiniteFetchProps<T, R> {
 
 export const useInfiniteFetch = <T, R>(args: UseInfiniteFetchProps<T, R>) => {
   const fetcher = useFetcher<R>();
-  const { page, per_page, search, goToNext, hasNextPage } = usePagination(
-    args.pagination,
-  );
-
-  const [data, setData] = useState<Record<number, T[]>>({
-    [page]: args?.initialData ?? [],
+  const [state, dispatch] = useReducer(infiniteFetchReducer<T>, {
+    ...args.pagination,
+    data: { [args.pagination.page]: args?.initialData ?? [] },
   });
 
   const fetchNextPage = () => {
     if (fetcher.state !== 'idle') return;
-    goToNext?.();
+
+    dispatch(goToNext());
   };
-  console.log(page, data);
+
   useEffect(() => {
-    if (data[page] !== undefined) return;
+    if (state.data[state.page] !== undefined) return;
 
     const urlWithParams = buildUrlWithParams(args.loaderUrl, {
-      page,
-      per_page,
-      search,
+      page: state.page,
+      per_page: state.per_page,
+      search: state.search,
     });
 
     fetcher.load(urlWithParams);
-  }, [page, per_page, search]);
+  }, [state.page, state.per_page, state.search]);
 
   useEffect(() => {
     const newData = args.dataExtractor(fetcher);
 
     if (newData && newData.length > 0) {
-      setData((prev) => ({ ...prev, [page]: newData }));
+      dispatch(setData(newData, state.page));
     }
   }, [fetcher.data]);
 
   const mergedData = useMemo(() => {
-    return merge(data);
-  }, [data]);
+    return merge(state.data);
+  }, [state.data]);
 
   const updateData = (cb: (data: T[]) => T[]) => {
-    setData((prev) => {
-      const newData = cb(merge(prev));
-
-      return splitByPage(newData, per_page);
-    });
+    dispatch(setState(cb(merge(state.data))));
   };
 
   return {
-    page,
-    per_page,
-    search,
-    hasNextPage,
+    ...state,
     fetchNextPage,
+    hasNextPage: state.totalPages - 1 > state.page,
     isFetchingNextPage: fetcher.state !== 'idle',
     data: mergedData,
     updateData,
   };
 };
+
+type Action<T> =
+  | {
+      type: 'SET_DATA';
+      payload: {
+        data: T[];
+        page: number;
+      };
+    }
+  | {
+      type: 'SET_STATE';
+      payload: {
+        data: T[];
+      };
+    }
+  | {
+      type: 'NEXT_PAGE';
+    };
+
+type InfiniteFetchReducerState<T = {}> = {
+  data: Record<number, T[]>;
+  page: number;
+  per_page: number;
+  totalItems: number;
+  totalPages: number;
+  search?: string;
+};
+
+export const infiniteFetchReducer = <T,>(
+  state: InfiniteFetchReducerState<T>,
+  action: Action<T>,
+): InfiniteFetchReducerState<T> => {
+  switch (action.type) {
+    case 'SET_DATA':
+      return {
+        ...state,
+        page: action.payload.page,
+        data: {
+          ...state.data,
+          [action.payload.page]: action.payload.data,
+        },
+      };
+    case 'SET_STATE':
+      const newData = splitByPage(action.payload.data, state.per_page);
+      return {
+        ...state,
+        page: getCurrentPage(newData),
+        data: newData,
+      };
+    case 'NEXT_PAGE':
+      return {
+        ...state,
+        page: state.page + 1,
+      };
+    default:
+      throw new Error('Invalid action');
+  }
+};
+
+function setData<T>(data: T[], page: number) {
+  return {
+    type: 'SET_DATA',
+    payload: {
+      data,
+      page,
+    },
+  } as const;
+}
+
+function setState<T>(data: T[]) {
+  return {
+    type: 'SET_STATE',
+    payload: {
+      data,
+    },
+  } as const;
+}
+
+function goToNext() {
+  return {
+    type: 'NEXT_PAGE',
+  } as const;
+}
 
 function merge<T>(data: Record<number, T[]>): T[] {
   return Object.values(data).reduce(
@@ -98,4 +172,8 @@ function splitByPage<T>(data: T[], per_page: number): Record<number, T[]> {
     },
     {} as Record<number, T[]>,
   );
+}
+
+function getCurrentPage<T>(data: Record<number, T[]>): number {
+  return Math.max(...Object.keys(data).map(Number));
 }
