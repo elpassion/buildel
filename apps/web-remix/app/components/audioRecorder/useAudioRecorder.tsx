045a5ce3f0948ae759f262597ec8879112a315d3
pawelsierant <paw9810@gmail.com>
2024-10-04 09:26:16 +0200

    Rollback

--- apps/web-remix/app/components/audioRecorder/useAudioRecorder.tsx
+++ apps/web-remix/app/components/audioRecorder/useAudioRecorder.tsx
@@ -44,6 +44,7 @@ export const useAudioRecorder = (props?: UseAudioRecorderProps) => {
 
   const requestPermissions = useCallback(async () => {
     if (typeof window === 'undefined') return;
+
     assert(navigator.mediaDevices, 'MediaDevices not supported');
 
     const stream = await navigator.mediaDevices.getUserMedia({
