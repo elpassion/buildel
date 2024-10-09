861eeb14fabd6f60adb7a7091dd2cc4098d347bb
breeg554 <dvdk98@gmail.com>
2024-10-09 15:08:23 +0200

    display toast on stripe callback

--- apps/web-remix/app/components/pages/settings/billing/page.tsx
+++ apps/web-remix/app/components/pages/settings/billing/page.tsx
@@ -1,7 +1,10 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import type { MetaFunction } from '@remix-run/node';
-import { useLoaderData } from '@remix-run/react';
+import { useLoaderData, useSearchParams } from '@remix-run/react';
 
+import { errorToast } from '~/components/toasts/errorToast';
+import { successToast } from '~/components/toasts/successToast';
+import { warningToast } from '~/components/toasts/warningToast';
 import { metaWithDefaults } from '~/utils/metadata';
 
 import { BillingPlanFilters, BillingPlanList } from './components/BillingPlan';
@@ -16,8 +19,43 @@ import {
 } from './components/SubscriptionCard';
 import type { loader } from './loader.server';
 
+const TOAST_DURATION = 10000;
+
 export function ProfileSettingsPage() {
-  const { user, plans } = useLoaderData<typeof loader>();
+  const { plans } = useLoaderData<typeof loader>();
+  const [searchParams, setSearchParams] = useSearchParams();
+
+  useEffect(() => {
+    const result = searchParams.get('result');
+
+    if (!result) return;
+
+    const id = setTimeout(() => {
+      if (result === 'success') {
+        successToast({
+          title: 'Subscription updated successfully',
+          duration: TOAST_DURATION,
+        });
+      } else if (result === 'error') {
+        errorToast({
+          title: 'Failed to update subscription',
+          duration: TOAST_DURATION,
+        });
+      } else if (result === 'cancel') {
+        warningToast({
+          title: 'Subscription has been cancelled',
+          duration: TOAST_DURATION,
+        });
+      }
+    }, 500);
+
+    searchParams.delete('result');
+    setSearchParams(searchParams);
+
+    return () => {
+      clearTimeout(id);
+    };
+  }, [searchParams]);
 
   return (
     <>
