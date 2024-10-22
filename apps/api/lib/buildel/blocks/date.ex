6256451ad49540cea892b3412dae7290a62a27e4
Michal Warda <t1du53k@gmail.com>
2024-10-22 09:02:20 +0200

    Trigger deploy

--- apps/api/lib/buildel/blocks/date.ex
+++ apps/api/lib/buildel/blocks/date.ex
@@ -48,7 +48,8 @@ defmodule Buildel.Blocks.Date do
   end
 
   defp get_date() do
-    DateTime.utc_now() |> DateTime.to_string()
+    DateTime.utc_now()
+    |> DateTime.to_string()
   end
 
   @impl true
