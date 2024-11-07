4591734b6b63896b408ea75a5063b5837e36f951
Michał <mw@grifel.dev>
2024-11-07 14:17:11 +0100

    Convert image input

--- apps/api/lib/buildel/blocks/image_input.ex
+++ apps/api/lib/buildel/blocks/image_input.ex
@@ -1,40 +1,22 @@
 defmodule Buildel.Blocks.ImageInput do
-  use Buildel.Blocks.Block
+  use Buildel.Blocks.NewBlock
 
-  @impl true
-  def options() do
-    %{
-      type: "image_input",
-      description:
-        "A streamlined module designed for the efficient handling and transmission of images.",
-      groups: ["image", "inputs / outputs"],
-      inputs: [Block.image_input("input", true)],
-      outputs: [Block.image_output()],
-      ios: [],
-      dynamic_ios: nil,
-      schema: schema()
-    }
+  defblock(:image_input,
+    description:
+      "A streamlined module designed for the efficient handling and transmission of images.",
+    groups: ["image", "inputs / outputs"]
+  )
+
+  definput(:input, schema: %{}, public: true, type: :image)
+  defoutput(:output, schema: %{}, type: :image)
+
+  def handle_input(:input, %Message{metadata: %{method: :delete}} = message, state) do
+    output(state, :output, message)
+    {:ok, state}
   end
 
-  @impl true
-  def schema() do
-    %{
-      "type" => "object",
-      "required" => ["name", "opts"],
-      "properties" => %{
-        "name" => name_schema(),
-        "opts" => options_schema()
-      }
-    }
-  end
-
-  @impl true
-  def handle_input("input", {_name, :text, file_id, %{method: :delete}}, state) do
-    output(state, "output", {:text, file_id}, %{metadata: %{method: :delete}})
-  end
-
-  @impl true
-  def handle_input("input", {_name, :binary, chunk, metadata}, state) do
-    output(state, "output", {:binary, chunk}, %{metadata: metadata})
+  def handle_input(:input, %Message{} = message, state) do
+    output(state, :output, message)
+    {:ok, state}
   end
 end
