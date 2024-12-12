d620b37b080521a14ff3fda4a4237dd54812dee3
breeg554 <dvdk98@gmail.com>
2024-12-12 11:42:17 +0100

    display input_name next to block inputs

--- apps/web-remix/app/components/pages/pipelines/build/editBlock/BlockInputList.tsx
+++ apps/web-remix/app/components/pages/pipelines/build/editBlock/BlockInputList.tsx
@@ -84,11 +84,11 @@ function BlockInputItem({ data, disabled }: IItem) {
     updateInputOptional(data, checked);
     setOptional(checked);
   };
-
+  console.log(data);
   return (
     <div className="relative grid grid-cols-[120px_60px_70px] gap-x-1">
       <p className="text-sm truncate" title={data.from.block_name}>
-        {data.from.block_name}
+        {`${data.from.block_name}:${data.to.input_name}`}
       </p>
       <div className="flex items-center text-sm">
         <CheckboxInput
